eb50df32061125fa2615d74776fd6b6d19aee6e9
rain84 <rainy_sky@mail.ru>
2021-02-08 15:32:56 +0300

    ✨ ♻️ Refatored tests and SLL. Added append-, prepend-, insert-methods

--- src/ds/LinkedList/SingleLinkedList.js
+++ src/ds/LinkedList/SingleLinkedList.js
@@ -1,97 +1,136 @@
 const makeNode = ({ val, next = null }) => ({ val, next })
 
 export class SingleLinkedList {
-	#head = null
-	#tail = null
-	#length = 0
+	head = null
+	tail = null
+	length = 0
 
 	constructor(...values) {
-		if (values[0] === undefined) return this
-
+		if (!values.length) return this
 		this.push.apply(this, values)
 	}
 
-	get length() {
-		return this.#length
+	get(index) {
+		let node = this.#getNodeNth(index)
+		if (!node) return
+
+		return node.val
 	}
 
-	get head() {
-		return this.#head
+	set(index, val) {
+		let node = this.#getNodeNth(index)
+		if (!node) return false
+		node.val = val
+
+		return true
 	}
 
-	get tail() {
-		return this.#tail
-	}
-
-	push(val, ...values) {
-		if (!val) return
-
-		// TODO:  maybe there is a more elegant solution
-		const node = makeNode({ val })
-		if (this.#tail) {
-			this.#tail.next = node
-			this.#tail = this.#tail.next
-		} else this.#tail = node
-
-		this.#length++
-
-		if (!this.#head) this.#head = this.#tail
-		if (values.length) this.push.apply(this, values)
+	push(...values) {
+		this.insert(this.length, values)
 	}
 
 	pop() {
-		if (!this.#length) return
+		if (!this.length) return
 
-		const { val } = this.#tail
+		const { val } = this.tail
 
-		if (this.#length === 1) {
-			this.#head = this.#tail = null
+		if (this.length === 1) {
+			this.head = this.tail = null
 		} else {
-			let node = this.#head
-			while (node?.next !== this.#tail) {
-				node = node.next
-			}
-			this.#tail = node
-			this.#tail.next = null
+			const node = this.#getNodeBefore(this.tail)
+			this.tail = node
+			this.tail.next = null
 		}
 
-		this.#length--
+		this.length--
 		return val
 	}
 
 	shift() {
-		if (!this.#length) return
+		if (!this.length) return
 
-		const { val } = this.#head
+		const { val } = this.head
 
-		if (this.#length === 1) {
-			this.#head = this.#tail = null
+		if (this.length === 1) {
+			this.head = this.tail = null
 		} else {
-			this.#head = this.#head.next
+			this.head = this.head.next
 		}
 
-		this.#length--
+		this.length--
 
 		return val
 	}
 
-	unshift(val, ...values) {
-		if (!val) return
+	unshift(...values) {
+		this.insert(0, values)
+	}
 
-		const node = makeNode({ val, next: this.#head })
-		this.#head = node
-		if (!this.#tail) this.#tail = this.#head
-		this.#length++
+	insert(index, values) {
+		if (values instanceof SingleLinkedList) {
+			return this.insert(index, [...values])
+		}
 
-		if (values.length) this.unshift.apply(this, values)
+		const isOutOfRange = index < 0 || this.length < index
+		const isEmptyValues = !values?.length
+		const { length } = values
+		let head, tail
+
+		if (isOutOfRange || isEmptyValues) return
+
+		head = tail = makeNode({ val: values.shift() })
+		for (const val of values) {
+			const node = makeNode({ val })
+			tail = tail.next = node
+		}
+
+		if (this.length === 0) {
+			Object.assign(this, { head, tail, length })
+		} else {
+			const prevNode = this.#getNodeNth(index - 1)
+
+			// is not starting node
+			if (prevNode) {
+				tail.next = prevNode.next
+				prevNode.next = head
+			} else {
+				tail.next = this.head
+				this.head = head
+			}
+
+			if (this.length === index) this.tail = tail
+			this.length += length
+		}
 	}
 
 	append(list) {
 		this.push(...list)
 	}
 
+	prepend(list) {
+		this.unshift(...list)
+	}
+
+	#getNodeNth(index) {
+		if (index < 0 || this.length < index) return null
+
+		let node = this.head
+		while (index--) node = node.next
+
+		return node
+	}
+
+	#getNodeBefore(nodeNext) {
+		if (!nodeNext) return null
+
+		let node = this.head
+		while (node.next !== nodeNext) node = node.next
+
+		return node
+	}
+
 	*[Symbol.iterator]() {
-		let node = this.#head
+		let node = this.head
 		while (node !== null) {
 			yield node.val
 			node = node.next
--- src/ds/LinkedList/singlelinkedlist.test.js
+++ src/ds/LinkedList/singlelinkedlist.test.js
@@ -18,6 +18,10 @@ describe('SingleLinkedList', () => {
 		expect(list.length).toBe(0)
 	})
 
+	test('should create SLT with values', () => {
+		expect([...list]).toMatchObject([1, 2, 3])
+	})
+
 	test('should push 1 value to the empty SLT', () => {
 		list = new SingleLinkedList()
 		list.push(42)
@@ -27,23 +31,6 @@ describe('SingleLinkedList', () => {
 		expect(list.head.next).toBe(null)
 		expect(list.head === list.tail).toBeTruthy()
 		expect(list.length).toBe(1)
-	})
-
-	test('should create SLT from array of values', () => {
-		const expexted = {
-			val: 1,
-			next: {
-				val: 2,
-				next: {
-					val: 3,
-					next: null
-				}
-			}
-		}
-
-		expect(list.head).toMatchObject(expexted)
-		expect(list.head.val).toBe(1)
-		expect(list.tail.val).toBe(3)
 	})
 
 	test('should have length', () => {
@@ -91,7 +78,7 @@ describe('SingleLinkedList', () => {
 		expect(list.length).toBe(4)
 		expect(list.head.val).toBe(6)
 
-		list.unshift(5, 4)
+		list.unshift(4, 5)
 		expect(list.length).toBe(6)
 		expect(list.head.val).toBe(4)
 		expect([...list]).toMatchObject([4, 5, 6, 1, 2, 3])
@@ -124,5 +111,122 @@ describe('SingleLinkedList', () => {
 		expect([...list]).toMatchObject([1, 2, 3, 4, 5, 6])
 		expect(list.tail.val).toBe(anotherList.tail.val)
 		expect(list.tail).not.toBe(anotherList.tail)
+	})
+
+	test('should get', () => {
+		expect(list.get(-1)).toBeUndefined()
+		expect(list.get(0)).toBe(1)
+		expect(list.get(2)).toBe(3)
+		expect(list.get(list.length)).toBeUndefined()
+
+		list.push(4, 5, 6)
+		expect(list.get(5)).toBe(6)
+
+		list.unshift(-3, -2, -1)
+		expect(list.get(0)).toBe(-3)
+		expect(list.get(3)).toBe(1)
+	})
+
+	test('should set', () => {
+		expect(list.set(-1, 42)).toBeFalsy()
+		expect(list.set(0, 42)).toBeTruthy()
+		expect(list.set(2, 43)).toBeTruthy()
+		expect(list.set(3, 27)).toBeFalsy()
+		expect(list.get(0)).toBe(42)
+		expect(list.get(2)).toBe(43)
+		expect(list.get(3)).toBeUndefined()
+	})
+
+	test('should insert values', () => {
+		list.insert(-1, [])
+		list.insert(0, [])
+		list.insert(list.length, [])
+		list.insert(list.length + 1, [])
+		expect([...list]).toMatchObject([1, 2, 3])
+
+		list.insert(list.length, [7, 8, 9])
+		expect([...list]).toMatchObject([1, 2, 3, 7, 8, 9])
+
+		list.insert(3, [4, 5, 6])
+		expect([...list]).toMatchObject([1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+		list.insert(0, [-2, -1, 0])
+		expect([...list]).toMatchObject([-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+		list.insert(3, [0.5])
+		expect([...list]).toMatchObject([-2, -1, 0, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+		expect(list.length).toBe(13)
+		list.insert(list.length, [10])
+		expect([...list]).toMatchObject([-2, -1, 0, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+	})
+
+	test('should insert another SLL', () => {
+		list.insert(-1, new SingleLinkedList())
+		list.insert(0, new SingleLinkedList())
+		list.insert(list.length, new SingleLinkedList())
+		list.insert(list.length + 1, new SingleLinkedList())
+		expect([...list]).toMatchObject([1, 2, 3])
+
+		list.insert(list.length, new SingleLinkedList(...[7, 8, 9]))
+		expect([...list]).toMatchObject([1, 2, 3, 7, 8, 9])
+
+		list.insert(3, new SingleLinkedList(...[4, 5, 6]))
+		expect([...list]).toMatchObject([1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+		list.insert(0, new SingleLinkedList(...[-2, -1, 0]))
+		expect([...list]).toMatchObject([-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+		list.insert(3, new SingleLinkedList(...[0.5]))
+		expect([...list]).toMatchObject([-2, -1, 0, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+		expect(list.length).toBe(13)
+		list.insert(list.length, new SingleLinkedList(...[10]))
+		expect([...list]).toMatchObject([-2, -1, 0, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+	})
+
+	test('should append another SLL', () => {
+		list.append(new SingleLinkedList())
+		expect([...list]).toMatchObject([1, 2, 3])
+
+		list.append(new SingleLinkedList(...[7, 8, 9]))
+		expect([...list]).toMatchObject([1, 2, 3, 7, 8, 9])
+	})
+
+	test('should prepend another SLL', () => {
+		list.prepend(new SingleLinkedList())
+		expect([...list]).toMatchObject([1, 2, 3])
+
+		list.prepend(new SingleLinkedList(...[7, 8, 9]))
+		expect([...list]).toMatchObject([7, 8, 9, 1, 2, 3])
+	})
+
+	xtest('should insert', () => {
+		let anotherList = new SingleLinkedList(4, 5, 6)
+		list.insert(-1, anotherList)
+		expect([...list]).toMatchObject([1, 2, 3])
+
+		list.insert(list.length + 1, anotherList)
+		expect([...list]).toMatchObject([1, 2, 3])
+
+		list.insert(3, anotherList)
+		expect([...list]).toMatchObject([1, 2, 3, 4, 5, 6])
+
+		list.insert(0, anotherList)
+		expect([...list]).toMatchObject([4, 5, 6, 1, 2, 3, 4, 5, 6])
+	})
+
+	xtest('should getNodeNth', () => {
+		list.push(4, 5, 6)
+		expect([
+			list.getNodeNth(-1),
+			list.getNodeNth(0).val,
+			list.getNodeNth(1).val,
+			list.getNodeNth(2).val,
+			list.getNodeNth(3).val,
+			list.getNodeNth(4).val,
+			list.getNodeNth(5).val,
+			list.getNodeNth(list.length)
+		]).toMatchObject([null, 1, 2, 3, 4, 5, 6, null])
 	})
 })
