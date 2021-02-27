b169ee78fc7d90e37115984065045726bec26c0b
rain84 <rainy_sky@mail.ru>
2021-02-28 00:27:39 +0300

    ✅ Update compose-tests

--- src/fp/compose.test.js
+++ src/fp/compose.test.js
@@ -1,15 +1,17 @@
 import { compose, composeRecursive } from './compose.js'
 
-test('compose', () => {
-	const sum = (a, b) => a + b
-	const mult = (a, b, c) => a * b * c
-	const double = (a) => a * 2
-	const minusOne = (a) => a - 1
-	const injectArgs = (...args) => (first) => [first, ...args]
+describe('Compose', () => {
+	test('should work', () => {
+		const sum = (a, b) => a + b
+		const mult = (a, b, c) => a * b * c
+		const double = (a) => a * 2
+		const minusOne = (a) => a - 1
+		const injectArgs = (...args) => (first) => [first, ...args]
 
-	let result = compose(mult, injectArgs(2, 2), double, minusOne, sum)(3, 2)
-	expect(result).toBe(32)
+		let result = compose(mult, injectArgs(2, 2), double, minusOne, sum)(3, 2)
+		expect(result).toBe(32)
 
-	result = composeRecursive(mult, injectArgs(2, 2), double, minusOne, sum)(3, 2)
-	expect(result).toBe(32)
+		result = composeRecursive(mult, injectArgs(2, 2), double, minusOne, sum)(3, 2)
+		expect(result).toBe(32)
+	})
 })
