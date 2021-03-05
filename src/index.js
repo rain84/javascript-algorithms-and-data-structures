6fa765eaa57c19bc3d9a00bca4cf613dc4c9585c
rain84 <rainy_sky@mail.ru>
2021-03-05 17:25:56 +0300

    Added .prettierrc Restructured folders Updated index.js for 'npm start'

--- src/index.js
+++ src/index.js
@@ -1,51 +1,51 @@
 import path from 'path'
 import { create } from 'ts-node'
-import { traverseDirRecursive } from './util/index.js'
+import { traverseDirRecursive } from './utils/index.js'
 import ts from 'typescript'
 
 const script = {
-	name: process.argv[2],
-	folderPath: path.resolve(process.cwd(), 'src')
+  name: process.argv[2],
+  folderPath: path.resolve(process.cwd(), 'src'),
 }
 
 if (!script.name) throw new Error('Script name should be provided')
 
 const execScript = async (script) => {
-	const action = async (entry) => {
-		const match = entry.path.match(/(.*)\.(js|ts)$/)
-		const isTarget = !!match?.[1]?.endsWith(script.name)
+  const action = async (entry) => {
+    const match = entry.path.match(/(.*)\.(js|mjs|ts)$/)
+    const isTarget = !!match?.[1]?.endsWith(script.name)
 
-		if (!isTarget) return
+    if (!isTarget) return
 
-		const is = {
-			js: match[2] === 'js',
-			ts: match[2] === 'ts'
-		}
+    const is = {
+      [match[2]]: true,
+    }
 
-		if (is.js) return import(entry.path)
-		if (is.ts) {
-			const tsNode = create()
+    if (is.js || is.mjs) return import(entry.path)
 
-			Object.assign(tsNode.config.options, {
-				isolatedModules: false,
-				emit: true
-			})
+    if (is.ts) {
+      const tsNode = create()
 
-			try {
-				tsNode.compile('', entry.path)
-				console.log('Successfully compiled\n')
-			} catch (e) {
-				console.log('Catched on ts-node compillation: \n', e)
-			}
-		}
-	}
-	return traverseDirRecursive(script.folderPath, action)
+      Object.assign(tsNode.config.options, {
+        isolatedModules: false,
+        emit: true,
+      })
+
+      try {
+        tsNode.compile('', entry.path)
+        console.log('Successfully compiled\n')
+      } catch (e) {
+        console.log('Catched on ts-node compillation: \n', e)
+      }
+    }
+  }
+  return traverseDirRecursive(script.folderPath, action)
 }
 
 try {
-	console.log('\n')
-	await execScript(script)
-	console.log('\n')
+  console.log('\n')
+  await execScript(script)
+  console.log('\n')
 } catch (e) {
-	console.log(e)
+  console.log(e)
 }
