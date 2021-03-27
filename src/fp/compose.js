5ec697ae03afad9342bded38f72cdb2f104b322c
rain84 <rainy_sky@mail.ru>
2021-03-27 14:26:43 +0300

    Added 2nd version of compose with reqursiion TODO: fix problem with args injecting

--- src/fp/compose.js
+++ src/fp/compose.js
@@ -1,16 +1,33 @@
 export const compose = (...fns) => (...args) =>
-	fns.reduceRight((args, fn) => fn(...(Array.isArray(args) ? args : [args])), args)
+    fns.reduceRight((args, fn) => fn(...(Array.isArray(args) ? args : [args])), args)
 
 export const composeRecursive = (...fns) => {
-	const last = fns.pop()
-	if (!fns.length) return (...args) => last(...args)
+    const last = fns.pop()
+    if (!fns.length) return (...args) => last(...args)
 
-	const prev = fns.pop()
-	fns.push((...args) => {
-		let res = last(...args)
-		if (!Array.isArray(res)) res = [res]
-		return prev(...res)
-	})
+    const prev = fns.pop()
+    fns.push((...args) => {
+        let res = last(...args)
+        if (!Array.isArray(res)) res = [res]
+        return prev(...res)
+    })
 
-	return composeRecursive(...fns)
+    return composeRecursive(...fns)
+}
+
+// TODO: fix case with injecting multiple arguments
+export const composeRecursive2 = (...fns) => (...args) => {
+    const wrapper = (fn) => (next) => (...args) => next(fn(...args))
+
+    const _compose = (...fns) => {
+        if (fns.length === 1) return fns[0]
+
+        const fn = wrapper(fns.pop())(fns.pop())
+        // const fn = wrapper(curry(fns.pop()))(curry(fns.pop()))
+        fns.push(fn)
+
+        return _compose(...fns)
+    }
+
+    return _compose(...fns)(...args)
 }
--- src/fp/compose.test.js
+++ src/fp/compose.test.js
@@ -1,17 +1,24 @@
-import { compose, composeRecursive } from './compose.js'
+import { compose, composeRecursive, composeRecursive2 } from './compose.js'
 
 describe('Compose', () => {
-	test('should work', () => {
-		const sum = (a, b) => a + b
-		const mult = (a, b, c) => a * b * c
-		const double = (a) => a * 2
-		const minusOne = (a) => a - 1
-		const injectArgs = (...args) => (first) => [first, ...args]
+    const sum = (a, b) => a + b
+    const mult = (a, b, c) => a * b * c
+    const double = (a) => a * 2
+    const minusOne = (a) => a - 1
+    const injectArgs = (...args) => (first) => [first, ...args]
 
-		let result = compose(mult, injectArgs(2, 2), double, minusOne, sum)(3, 2)
-		expect(result).toBe(32)
+    test('compose should work', () => {
+        const result = compose(mult, injectArgs(2, 2), double, minusOne, sum)(3, 2)
+        expect(result).toBe(32)
+    })
 
-		result = composeRecursive(mult, injectArgs(2, 2), double, minusOne, sum)(3, 2)
-		expect(result).toBe(32)
-	})
+    test('composeRecursive should work', () => {
+        const result = composeRecursive(mult, injectArgs(2, 2), double, minusOne, sum)(3, 2)
+        expect(result).toBe(32)
+    })
+
+    xtest('composeRecursive2 should work', () => {
+        const result = composeRecursive2(mult, injectArgs(2, 2), double, minusOne, sum)(3, 2)
+        expect(result).toBe(32)
+    })
 })
