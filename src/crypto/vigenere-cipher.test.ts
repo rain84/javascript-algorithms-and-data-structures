e492c0f1f86b7acdfb8f8878098d724bbb84bcf2
rain84 <rainy_sky@mail.ru>
2021-05-10 15:03:21 +0300

    ✨ Added vigenere cipher ✅ Added tests

---
+++ src/crypto/vigenere-cipher.test.ts
@@ -0,0 +1,19 @@
+import { encode, decode, alphabet } from './vigenere-cipher'
+
+describe('Poly-alphabet "Vigenere"-cipher', () => {
+    const keyword = 'шифр'
+    const message = 'ЭТОСООБЩЕНИЕНУЖНОЗАКОДИРОВАТЬ'.toLocaleLowerCase()
+    const encodedMessage = 'хыгвжчхйэцэхёьыюжрфыжмэбжкфгф'
+
+    test('should encode', () => {
+        expect(encode(alphabet)(message, keyword)).toBe(encodedMessage)
+    })
+
+    test('should decode', () => {
+        expect(decode(alphabet)(encodedMessage, keyword)).toBe(message)
+    })
+
+    test('should just work', () => {
+        expect(decode(alphabet)(encode(alphabet)(message, keyword), keyword)).toBe(message)
+    })
+})
---
+++ src/crypto/vigenere-cipher.ts
@@ -0,0 +1,51 @@
+type CryptoFn = (iMessage: number, iKeyword: number, length: number) => number
+
+interface Algorithm {
+    encode: CryptoFn
+    decode: CryptoFn
+}
+const crypto: Algorithm = {
+    encode: (iMessage, iKeyword, length) => (iMessage + iKeyword) % length,
+    decode: (iMessage, iKeyword, length) => (iMessage - iKeyword + length) % length,
+}
+
+export const alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+const execute = (cryptoFn: CryptoFn) => (alphabet: string) => (message: string, keyword: string): string => {
+    alphabet = alphabet.toLocaleLowerCase()
+    message = message.toLocaleLowerCase()
+    keyword = keyword.toLocaleLowerCase()
+
+    const result = []
+
+    const ch = {
+        message: '',
+        keyword: '',
+    }
+
+    const index = {
+        message: -1,
+        keyword: -1,
+        result: -1,
+    }
+
+    for (let i = 0; i < message.length; i++) {
+        ch.message = message[i]
+        ch.keyword = keyword[i % keyword.length]
+
+        index.keyword = alphabet.indexOf(ch.keyword)
+        index.message = alphabet.indexOf(ch.message)
+
+        if (index.keyword === -1) throw new Error('Keyword is not valid for alphabet')
+        if (index.message === -1) throw new Error('Message is not valid for alphabet')
+
+        index.result = cryptoFn(index.message, index.keyword, alphabet.length)
+
+        result.push(alphabet[index.result])
+    }
+
+    return result.join('')
+}
+
+export const encode = execute(crypto.encode)
+export const decode = execute(crypto.decode)
