e86441af9db25cf7b12ae8c1ab7bc194dabeea2d
rain84 <rainy_sky@mail.ru>
2021-11-16 20:42:28 +0300

    feat: added solution for snapchat-task

---
+++ src/interview-tasks/snapchat/snapchat-1.ts
@@ -0,0 +1,46 @@
+/*
+Вопрос от Snapchat 
+
+Given a list of possibly overlapping intervals,
+return a new list of intervals where all overlapping intervals have been merged.
+
+The input list is not necessarily ordered in any way.
+For example, given (1, 3), (5, 8), (4, 10), (20, 25), you should return (1, 3), (4, 10), (20, 25).
+*/
+
+type Maybe<T> = T | null
+
+type Interval = [Number, Number]
+type Intervals = Interval[]
+const mergeOverlappingIntervals = ([first, ...rest]: Intervals): Intervals => {
+  if (rest.length === 1) return rest
+
+  const interval: Maybe<Interval> = rest.reduce((overlapper, current) => {
+    const canOverlapFromLeft = overlapper[0] < current[0] && overlapper[1] > current[0] && overlapper[1] < current[1]
+    const canOverlapFromRight = overlapper[1] > current[1] && overlapper[0] > current[0] && overlapper[0] < current[1]
+    const isOverlapped = overlapper[0] > current[0] && overlapper[1] < current[1]
+
+    if (canOverlapFromLeft) return [overlapper[0], current[1]]
+    if (canOverlapFromRight) return [current[0], overlapper[1]]
+    if (isOverlapped) return current
+
+    return overlapper
+  }, first)
+
+  return interval ? [interval, ...mergeOverlappingIntervals(rest)] : [...mergeOverlappingIntervals(rest)]
+}
+
+const input: Intervals = [
+  [1, 3],
+  [5, 8],
+  [4, 10],
+  [20, 25],
+]
+const output: Intervals = [
+  [1, 3],
+  [4, 10],
+  [20, 25],
+]
+
+const result = mergeOverlappingIntervals(input)
+console.log(JSON.stringify(result, null, ''))
---
+++ src/interview-tasks/snapchat/snapchat-2.ts
@@ -0,0 +1,18 @@
+// Snapchat
+
+// Given an array of time intervals(start, end) for classroom lectures(possibly overlapping),
+// find the minimum number of rooms required.
+
+// For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.
+
+//  TODO: implement this later
+
+/*
+Вопрос от Snapchat 
+
+Given a list of possibly overlapping intervals,
+return a new list of intervals where all overlapping intervals have been merged.
+
+The input list is not necessarily ordered in any way.
+For example, given (1, 3), (5, 8), (4, 10), (20, 25), you should return (1, 3), (4, 10), (20, 25).
+*/
