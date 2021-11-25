d8c9556e3e7b0a480bc859b5f2c15a7bb6da12e8
rain84 <rainy_sky@mail.ru>
2021-11-25 21:45:39 +0300

    ✨ feat: added Tree - DS implementation feat: added breadth first search feat: added depth first search

--- src/ds/tree/tree.ts
+++ src/ds/tree/tree.ts
@@ -1,56 +1,87 @@
 /*
-Given a tree where each edge has a weight,
-compute the length of the longest path in the tree.
-For example, given the following tree:
+  Tree implementation.
 
-   a
-  /|\
- b c d
-    / \
-   e   f
-  / \
- g   h
+  1. bfs(cb) - Breadth First Search
+  2. dfs(cb) - Depth First Search by pre-order
 
-and the weights: a - b: 3, a - c: 5, a - d: 8, d - e: 2, d - f: 4, e - g: 1, e - h: 1,
-the longest path would be c -> a -> d -> f, with a length of 17.
-
-The path does not have to pass through the root,
-and each node can have any amount of children.
+  cb(value_of_node): returned 'false' will abort traversing the tree
 */
 
-interface INode<T> {
-  payload: T
-  children?: INode<T>[] & ITree<T>[]
-}
+type Cb<T> = ((value: T) => boolean | void)
 
 interface ITree<T> {
+  value: T
+  children?: ITree<T>[]
   add(...nodes: ITree<T>[]): ITree<T>
+  bfs(cb: Cb<T>): void | boolean
+  dfs(cb: Cb<T>): void | boolean
 }
 
-class Tree<T> implements ITree<T> {
-  node: INode<T>
+export class Tree<T> implements ITree<T> {
+  children?: ITree<T>[]
 
-  constructor(public payload: T) {}
+  constructor(public value: T) {}
 
   add(...nodes: ITree<T>[]): ITree<T> {
-    if (!this.node.children) this.node.children = []
-    this.node.children.push(...nodes)
+    if (!this.children) this.children = []
+    this.children.push(...nodes)
 
     return this
   }
+
+  bfs(cb: Cb<T>) {
+    this.bfs2([this], cb)
+  }
+
+  private bfs2(nodes: ITree<T>[], cb: Cb<T>) {
+    const childs: ITree<T>[] = []
+
+    for (const node of nodes) {
+      if (cb(node.value) === false) return false
+      if (node.children) childs.push(...node.children)
+    }
+
+    if (!childs.length) return
+    if (this.bfs2(childs, cb) === false) return false
+  }
+
+  dfs(cb: Cb<T>) {
+    this.dfs2(this, cb)
+  }
+
+  private dfs2(node: ITree<T>, cb: Cb<T>) {
+    if (cb(node.value) === false) return false
+    if (!node.children) return
+
+    for (const child of node.children) {
+      if (this.dfs2(child, cb) === false) return false
+    }
+  }
 }
 
+//      TREE
+//       0
+//     /   \
+//   1      2
+//  / \    /  \
+// 3   4  5    6
+//            / \
+//           7   8
+
 // prettier-ignore
-const tree = new Tree('a').add(
+const tree = new Tree(0).add(
+  new Tree(1).add(
     new Tree(3),
+    new Tree(4),
+  ),
+  new Tree(2).add(
     new Tree(5),
-    new Tree(8).add(
-      new Tree(2).add(
-        new Tree(1),
-        new Tree(1)),
-      new Tree(4)
-    )
-  )
+    new Tree(6).add(
+      new Tree(7),
+      new Tree(8),
+    ),
+  ),
+)
 
 function stringify(data: any) {
   return JSON.stringify(data, null, ' ')
@@ -61,4 +92,18 @@ function log(data: any, msg?: string): void {
   console.log(msg, isObject ? stringify(data) : data)
 }
 
-log(tree)
+// log(tree)
+
+const cb1 = (val: number) => console.log(val)
+const cb2 = (val: number) => {
+  console.log(val)
+  if (val === 5) return false
+}
+
+console.log('\nBFS\n')
+tree.bfs(cb1)
+tree.bfs(cb2)
+
+console.log('\nDFS\n')
+tree.dfs(cb1)
+tree.dfs(cb2)
