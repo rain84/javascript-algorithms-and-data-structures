4a00ef090ddb5050b8330ba0b885f38160af450b
rain84 <rainy_sky@mail.ru>
2022-02-04 20:57:25 +0300

    feat: added maxUniqSubstring

---
+++ src/strings/maxUniqSubstring.ts
@@ -0,0 +1,31 @@
+const maxUniqSubstring = (str: string) => {
+  let cache = new Set<string>()
+  const max = {
+    pointers: { l: 0, r: 0 },
+    length: 0,
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    const ch = str[i]
+
+    if (cache.has(ch)) {
+      if (max.length < cache.size) {
+        max.length = cache.size
+        max.pointers.l = i - cache.size
+        max.pointers.r = i
+        cache.clear()
+      }
+    }
+    cache.add(ch)
+  }
+
+  const maxSubstr = max.length ? str.slice(max.pointers.l, max.pointers.r) : str
+  return maxSubstr
+}
+
+console.log(`
+  hellothere: ${maxUniqSubstring('hellothere')}
+  helot: ${maxUniqSubstring('helot')}
+`)
+
+export {}
