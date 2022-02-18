5851d1a7a496ff32a364deb1c17132b12e636171
rain84 <rainy_sky@mail.ru>
2022-02-18 21:40:09 +0300

    feat: queue DS implemented test: added queue tests

---
+++ src/ds/queue.test.ts
@@ -0,0 +1,29 @@
+import { Queue } from './queue'
+
+describe('Queue', () => {
+  let queue: Queue<string>
+
+  beforeEach(() => {
+    queue = new Queue(...'abcde')
+  })
+
+  test('Should have ctor, "isEmpty()" & "size"', () => {
+    expect(queue.size).toBe(5)
+    expect(queue.isEmpty()).toBeFalsy()
+
+    queue = new Queue()
+    expect(queue.size).toBe(0)
+    expect(queue.isEmpty()).toBeTruthy()
+  })
+
+  test('Should have "enqueue()" & "dequeue()"', () => {
+    expect((queue.enqueue('z'), queue.size)).toBe(6)
+    expect(queue.dequeue()).toBe('a')
+    expect(queue.size).toBe(5)
+
+    queue.dequeue(), queue.dequeue(), queue.dequeue(), queue.dequeue()
+    expect(queue.dequeue()).toBe('z')
+    expect(queue.size).toBe(0)
+    expect(queue.isEmpty()).toBeTruthy()
+  })
+})
---
+++ src/ds/queue.ts
@@ -0,0 +1,54 @@
+class Node<T> {
+  constructor(
+    public val: T,
+    public prev: Node<T> | null = null
+  ) { }
+}
+
+export class Queue<T> {
+  #size_ = 0
+  #head: Node<T> | null = null
+  #tail: Node<T> | null = null
+
+  constructor(...values: T[]) {
+    if (values.length) {
+      values.forEach((val) => this.enqueue(val))
+    }
+  }
+
+  get size() {
+    return this.#size_
+  }
+
+  enqueue(val: T) {
+    const node = new Node(val)
+
+    if (this.isEmpty()) {
+      this.#head = this.#tail = node
+    } else {
+      ;(<Node<T>>this.#head).prev = node
+      this.#head = node
+
+      if (this.#tail === null) this.#tail = node
+    }
+
+    this.#size_++
+
+    return this
+  }
+
+  dequeue() {
+    if (this.#tail === null) return
+
+    const { val } = this.#tail
+    this.#tail = this.#tail.prev
+    this.#size_--
+
+    return val
+  }
+
+  isEmpty() {
+    return this.size === 0
+  }
+}
+
