9385d59ce40c79a1314e7137b412bab64ed6db8c
rain84 <rainy_sky@mail.ru>
2022-02-27 01:00:34 +0300

    ✨ feat: added "delete"-method ♻️ refact: some methods refatored ✅ test: tests updated

--- src/ds/hash_table.test.ts
+++ src/ds/hash_table.test.ts
@@ -2,18 +2,74 @@ import { HashTable } from './hash_table'
 
 describe('DS: HashTable', () => {
   let hashTable: HashTable<number>
+  const data =
+    'The C Programming Language is a computer programming book\
+   written by Brian Kernighan and Dennis Ritchie'.split(
+      /\s+/
+    )
 
   beforeEach(() => {
     hashTable = new HashTable()
+    data.forEach((key, val) => hashTable.set(key, val))
   })
 
-  test('Should have "set()"', () => {
-    hashTable.set('Hello', 7)
-    hashTable.set('world', 42)
-    hashTable.set('!', 25)
+  test('Should have "set()" and "get()"', () => {
+    expect(hashTable.get('The')).toBe(0)
+    expect(hashTable.get('Language')).toBe(3)
+    expect(hashTable.get('non-existent')).toBeUndefined()
 
-    expect(hashTable.get('Hello')).toBe(7)
-    expect(hashTable.get('world')).toBe(42)
-    expect(hashTable.get('!')).toBe(25)
+    hashTable.set('prop', 42)
+    expect(hashTable.get('prop')).toBe(42)
+    hashTable.set('prop', 57)
+    expect(hashTable.get('prop')).toBe(57)
+
+    hashTable = new HashTable(1)
+    hashTable.set('prop1', 1)
+    hashTable.set('prop2', 2)
+    hashTable.set('prop3', 3)
+    expect(hashTable.get('prop1')).toBe(1)
+    expect(hashTable.get('prop2')).toBe(2)
+    expect(hashTable.get('prop3')).toBe(3)
+  })
+
+  test('Should have "delete()"', () => {
+    hashTable.delete('The')
+    expect(hashTable.get('The')).toBeUndefined()
+
+    hashTable.delete('non-existent')
+    expect(hashTable.get('non-existent')).toBeUndefined()
+
+    hashTable = new HashTable(1)
+    hashTable.set('prop1', 1)
+    hashTable.set('prop2', 2)
+    hashTable.set('prop3', 3)
+    hashTable.delete('prop2')
+    expect(hashTable.get('prop1')).toBe(1)
+    expect(hashTable.get('prop2')).toBeUndefined()
+    expect(hashTable.get('prop3')).toBe(3)
+  })
+
+  test('Should have "keys()"', () => {
+    const keys = hashTable.keys()
+    keys.sort()
+    data.sort()
+
+    expect(keys).toMatchObject(data)
+  })
+
+  test('Should have "values()"', () => {
+    const indexes = data.map((_, i) => i)
+    const values = hashTable.values()
+    values.sort((a, b) => a - b)
+
+    expect(values).toMatchObject(indexes)
+  })
+
+  test('Should have "entries()"', () => {
+    const entries = hashTable
+      .entries()
+      .reduce((acc, [val, key]) => ((acc[key] = val), acc), [])
+
+    expect(entries).toMatchObject(data)
   })
 })
--- src/ds/hash_table.ts
+++ src/ds/hash_table.ts
@@ -1,4 +1,5 @@
 type Items<T> = [string, T][]
+type Collection = 'keys' | 'values' | 'entries'
 
 export class HashTable<T> {
   #storage: Items<T>[]
@@ -6,17 +7,63 @@ export class HashTable<T> {
 
   constructor(max_length = 51) {
     this.#MAX_LENGTH = max_length
-    this.#storage = new Array(max_length).fill([])
+    this.#storage = new Array(max_length)
   }
 
   get(key: string) {
-    const index = this.#hash(key)
-    return this.#storage[index].find(([itemKey]) => itemKey === key)?.[1]
+    return this.#getItem(key)?.[1]
   }
 
   set(key: string, val: T) {
+    const item = this.#getItem(key)
+
+    if (item) item[1] = val
+    else {
+      const index = this.#hash(key)
+      if (!this.#storage[index]) this.#storage[index] = [[key, val]]
+      else this.#storage[index].push([key, val])
+    }
+  }
+
+  delete(key: string) {
     const index = this.#hash(key)
-    this.#storage[index].push([key, val])
+
+    if (!this.#storage[index]) return
+    if (this.#storage[index].length === 1) delete this.#storage[index]
+    else
+      this.#storage[index] = this.#storage[index].filter(
+        ([itemKey]) => itemKey !== key
+      )
+  }
+
+  keys() {
+    return this.#getCollectionOf('keys')
+  }
+
+  values() {
+    return this.#getCollectionOf('values')
+  }
+
+  entries() {
+    return this.#getCollectionOf('entries')
+  }
+
+  #getItem(key: string) {
+    const index = this.#hash(key)
+    return this.#storage[index]?.find(([itemKey]) => itemKey === key)
+  }
+
+  #getCollectionOf(type: Collection) {
+    return this.#storage.flat().reduce((collection, [key, val]) => {
+      let item: any
+
+      if (type === 'keys') item = key
+      else if (type === 'values') item = val
+      else item = [key, val]
+
+      collection.push(item)
+      return collection
+    }, [] as any[])
   }
 
   #hash(s: string) {
