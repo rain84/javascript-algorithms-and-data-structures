711367e28c99b09309aa018f5074d12871039703
rain84 <rainy_sky@mail.ru>
2022-05-27 14:27:38 +0300

    ✨ feat: experiments with TS-types

---
+++ src/ts-types/index.ts
@@ -0,0 +1,29 @@
+type Input = string | number
+type Res<T extends Input> = T extends string ? string : number
+type Res2 = string | number
+
+const fn = <T extends Input>(data: T): Res2 => {
+  return data
+}
+
+type MessaageOf<T> = T extends { message: unknown } ? T['message'] : never
+
+interface Shape {
+  message: 'sms' | 'udp'
+  id: number
+}
+
+type M = MessaageOf<Shape>
+type M2 = MessaageOf<7>
+
+type Flatten<T> = T extends Array<infer U> ? U : T
+type F1 = Flatten<string[]>
+type F2 = Flatten<number>
+
+type ReturnType2<T> = T extends (...args: any[]) => infer R ? R : never
+type R1 = ReturnType2<(a: number) => string>
+type R2 = ReturnType2<number>
+
+type ToArray<T> = [T] extends any ? T[] : never
+type T1 = ToArray<string>
+type T2 = ToArray<string | number>
