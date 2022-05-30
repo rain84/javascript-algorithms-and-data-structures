b75d96e4dbde9fa61bf29fa631c56b74041ac9d1
rain84 <rainy_sky@mail.ru>
2022-05-30 21:38:49 +0300

    chore: upgrade from JS to TS

---
+++ src/sorting/radixSort.ts
@@ -0,0 +1,30 @@
+/**
+    Radix Sort:
+    
+    Time Complexity : Best & Worst & Average => O(n * k)
+    Space Complexity: O(n + k) 
+    Stablitiy       : ??
+    Is-In-Place     : ?? (probably no)
+    When to use     : ??
+    Remark          : ??
+*/
+// FIXME sorting is broken on the negative values
+import { getDigit, getLength } from '../utils/math.js'
+
+export const radixSort = (arr: number[]) => {
+  const buckets = []
+  let iterations = Math.max(...arr.map(getLength))
+  let position = 1
+
+  while (iterations--) {
+    for (let i = 0; i < 10; i++) buckets[i] = <number[]>[]
+    for (const num of arr) {
+      buckets[getDigit(num, position)].push(num)
+    }
+
+    arr = buckets.flat()
+    position++
+  }
+
+  return arr
+}
