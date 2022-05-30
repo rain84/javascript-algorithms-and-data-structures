d80e276f4349d862cb8d5d1ed611fd5a62d3cd47
rain84 <rainy_sky@mail.ru>
2022-05-30 21:36:27 +0300

    🏷️ chore: upgrade from TS to JS

---
+++ src/explore-es-features/proxy.ts
@@ -0,0 +1,21 @@
+interface ITarget<T = number> {
+  [K: string]: T
+}
+const target: ITarget = {
+  val: 7,
+}
+
+const handler: ProxyHandler<ITarget> = {
+  get: (target: ITarget, name: string) => target[name],
+
+  set: (target: ITarget, prop: string, val: number) => {
+    console.log('set', prop, val)
+    target[prop] = val
+    return true
+  },
+}
+
+const p = new Proxy(target, handler)
+
+console.log(++p.val)
+console.log(++p.val)
