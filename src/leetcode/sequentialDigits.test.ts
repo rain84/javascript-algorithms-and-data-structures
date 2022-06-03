257eac8fe42531492fb71c016b29aec8f7ef2030
rain84 <rainy_sky@mail.ru>
2022-06-03 20:18:22 +0300

    ✨ ✅ feat: added getSequentialDigits

---
+++ src/leetcode/sequentialDigits.test.ts
@@ -0,0 +1,26 @@
+import { getSequentialDigits, nextSequential } from './sequentialDigits'
+
+describe('Interview: Amazon. Sequential Digits', () => {
+  test('getSequentialDigits should work', () => {
+    expect(getSequentialDigits(100, 300)).toMatchObject([123, 234])
+    expect(getSequentialDigits(1000, 13000)).toMatchObject([
+      1234, 2345, 3456, 4567, 5678, 6789, 12345,
+    ])
+    expect(getSequentialDigits(1000, 13000)).toMatchObject([
+      1234, 2345, 3456, 4567, 5678, 6789, 12345,
+    ])
+    expect(getSequentialDigits(234, 2314)).toMatchObject([
+      234, 345, 456, 567, 678, 789, 1234,
+    ])
+    expect(getSequentialDigits(89, 234)).toMatchObject([89, 123, 234])
+  })
+
+  test('nextSequential should work', () => {
+    expect(nextSequential(9)).toBeUndefined()
+    expect(nextSequential(10)).toBe(12)
+    expect(nextSequential(13)).toBe(23)
+    expect(nextSequential(6789)).toBe(6789)
+    expect(nextSequential(72345)).toBe(123456)
+    expect(nextSequential(10 ** 9 + 1)).toBeUndefined()
+  })
+})
---
+++ src/leetcode/sequentialDigits.ts
@@ -0,0 +1,50 @@
+// https://leetcode.com/problems/sequential-digits/
+
+// An integer has sequential digits
+// if and only if each digit in the number
+// is one more than the previous digit.
+
+// Return a sorted list of all the integers
+// in the range[low, high]
+// inclusive that have sequential digits.
+
+export const nextSequential = (n: number) => {
+  if (n < 10 || 10 ** 9 < n) return
+
+  const str = n.toString()
+  let digit = +str.at(0)!
+
+  const sequence1: number[] = []
+  const sequence2: number[] = []
+  const sequence3: number[] = []
+
+  for (let i = 0, digit2 = digit + 1; i < str.length; i++, digit++, digit2++) {
+    if (digit < 10) sequence1.push(digit)
+    if (digit2 < 10) sequence2.push(digit2)
+    sequence3.push(i + 1)
+  }
+  sequence3.push(sequence3.at(-1)! + 1)
+
+  const result = {
+    sequence1: +sequence1.join(''),
+    sequence2: +sequence2.join(''),
+    sequence3: +sequence3.join(''),
+  }
+
+  if (n <= result.sequence1) return result.sequence1
+  else if (n <= result.sequence2) return result.sequence2
+
+  return result.sequence3
+}
+
+export const getSequentialDigits = (min: number, max: number) => {
+  const result: number[] = []
+
+  if (min >= max) return result
+
+  let sequence = min
+
+  while ((sequence = nextSequential(sequence)!) <= max) result.push(sequence++)
+
+  return result
+}
