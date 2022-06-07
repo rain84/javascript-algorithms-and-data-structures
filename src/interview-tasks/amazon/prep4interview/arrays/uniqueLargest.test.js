33861c0f4b9bc08b149e3b28445158cd7dc50719
rain84 <rainy_sky@mail.ru>
2022-06-07 18:10:12 +0300

    ✨ ✅ feat: uniqueLargest & largestAndSecondLargest

---
+++ src/interview-tasks/amazon/prep4interview/arrays/uniqueLargest.test.js
@@ -0,0 +1,18 @@
+import { largestAndSecondLargest as l, uniqueLargest } from './uniqueLargest'
+import { input } from './input'
+
+describe('Interview: Amazon. Arrays. uniqueLargest & largestAndSecondLargest', () => {
+  test('largestAndSecondLargest should work', () => {
+    expect(l(3, [2, 1, 2])).toMatchObject([2, 1])
+    expect(l(6, [5, 1, 2, 3, 4, 5])).toMatchObject([5, 4])
+    expect(l(6, [5, 1, 2, 3, 4, 5])).toMatchObject([5, 4])
+    expect(l(6, input)).toMatchObject([32760, 32759])
+  })
+
+  test('uniqueLargest should work', () => {
+    expect(uniqueLargest(3, [2, 1, 2])).toMatchObject([2, 1, -1])
+    expect(uniqueLargest(2, [5, 1, 2, 3, 4, 5])).toMatchObject([5, 4])
+    expect(uniqueLargest(3, [5, 1, 2, 3, 4, 5])).toMatchObject([5, 4, 3])
+    expect(uniqueLargest(3, input)).toMatchObject([32760, 32759, 32758])
+  })
+})
