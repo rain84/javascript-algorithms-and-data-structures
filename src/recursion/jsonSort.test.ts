ca731a1988b97bb34bfbccaa60ee925e08752e50
rain84 <rainy_sky@mail.ru>
2022-11-17 20:25:32 +0300

    test: :label: Migrated from JS to TS

--- src/recursion/jsonSort.test.ts
+++ src/recursion/jsonSort.test.ts
@@ -1,37 +1,39 @@
 import { jsonSort } from './jsonSort'
 
-describe('JsonSort', () => {
-  it('should work', () => {
-    expect(jsonSort(input)).toMatchObject(output)
+const input = {
+  'breadcrumb.activeSelectionForeground': '#c4db15',
+  'editorCursor.foreground': '#f00',
+  'tab.activeBorderTop': '#f00',
+  'editorLineNumber.activeForeground': '#f00',
+  'minimap.findMatchHighlight': '#f00',
+  activityBar: {
+    foreground: '#f00',
+    activeBorder: '#f00',
+  },
+  'tab.activeBorder': '#f00',
+  'panelTitle.activeBorder': '#f00',
+  'tab.activeModifiedBorder': '#f00',
+}
+
+const output = {
+  activityBar: {
+    activeBorder: '#f00',
+    foreground: '#f00',
+  },
+  'breadcrumb.activeSelectionForeground': '#c4db15',
+  'editorCursor.foreground': '#f00',
+  'editorLineNumber.activeForeground': '#f00',
+  'minimap.findMatchHighlight': '#f00',
+  'panelTitle.activeBorder': '#f00',
+  'tab.activeBorder': '#f00',
+  'tab.activeBorderTop': '#f00',
+  'tab.activeModifiedBorder': '#f00',
+}
+
+describe('Recursion', () => {
+  describe('JsonSort', () => {
+    it('should work', () => {
+      expect(jsonSort(input)).toMatchObject(output)
+    })
   })
 })
-
-var input = {
-  'breadcrumb.activeSelectionForeground': '#c4db15',
-  'editorCursor.foreground': '#f00',
-  'tab.activeBorderTop': '#f00',
-  'editorLineNumber.activeForeground': '#f00',
-  'minimap.findMatchHighlight': '#f00',
-  activityBar: {
-    foreground: '#f00',
-    activeBorder: '#f00',
-  },
-  'tab.activeBorder': '#f00',
-  'panelTitle.activeBorder': '#f00',
-  'tab.activeModifiedBorder': '#f00',
-}
-
-var output = {
-  activityBar: {
-    activeBorder: '#f00',
-    foreground: '#f00',
-  },
-  'breadcrumb.activeSelectionForeground': '#c4db15',
-  'editorCursor.foreground': '#f00',
-  'editorLineNumber.activeForeground': '#f00',
-  'minimap.findMatchHighlight': '#f00',
-  'panelTitle.activeBorder': '#f00',
-  'tab.activeBorder': '#f00',
-  'tab.activeBorderTop': '#f00',
-  'tab.activeModifiedBorder': '#f00',
-}
