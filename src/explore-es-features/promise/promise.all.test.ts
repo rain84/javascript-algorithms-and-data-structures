ba7c5cc820465900951afc03e21eea9da26ecf0a
rain84 <rainy_sky@mail.ru>
2022-11-17 18:59:24 +0300

    feat: :white_check_mark: added tests for Promise.all() and Promise.race()

---
+++ src/explore-es-features/promise/promise.all.test.ts
@@ -0,0 +1,72 @@
+import { resolve, reject } from '../../utils/async'
+import { Promise_ } from './promise'
+
+type TPromises = Record<string, Array<unknown | Promise<unknown>>>
+
+describe('Promise', () => {
+  // prettier-ignore
+  const expectedPromises: TPromises = ({
+    allResolved: [resolve(1, 2),resolve(2, 3),resolve(3, 1),],
+    firstRejected: [reject(4, 2),resolve(5, 3),resolve(6, 1),],
+    secondRejected: [resolve(7, 2),reject(8, 3),resolve(9, 1),],
+    allRejected: [reject(10, 2), reject(11, 3), reject(12, 1),],
+    values: [2, 4, 6],
+    mixedValues: [2, 4, 6, resolve(1, 1), resolve(3, 2)],
+  })
+
+  const waitForAllSettled = async () => {
+    const promises = Object.values(expectedPromises)
+      .filter(Boolean)
+      .reduce((acc, val) => {
+        if (Array.isArray(val)) acc.push(...val)
+        else acc.push(val)
+        return acc
+      }, [])
+
+    await Promise.allSettled(promises)
+  }
+
+  describe('promise.all()', () => {
+    beforeAll(waitForAllSettled)
+
+    it('should resolve all', async () => {
+      expect.assertions(1)
+      return expect(
+        Promise_.all(expectedPromises.allResolved)
+      ).resolves.toEqual([1, 2, 3])
+    })
+
+    it('should reject 1st', async () => {
+      expect.assertions(1)
+      return expect(Promise_.all(expectedPromises.firstRejected)).rejects.toBe(
+        4
+      )
+    })
+
+    it('should reject 2st', async () => {
+      expect.assertions(1)
+      return expect(Promise_.all(expectedPromises.secondRejected)).rejects.toBe(
+        8
+      )
+    })
+
+    it('should reject all', async () => {
+      expect.assertions(1)
+      return expect(Promise_.all(expectedPromises.allRejected)).rejects.toBe(10)
+    })
+
+    it('should resolve non-promise values', async () => {
+      expect.assertions(1)
+      return expect(Promise_.all(expectedPromises.values)).resolves.toEqual([
+        2, 4, 6,
+      ])
+    })
+
+    it('should resolve mixed values', async () => {
+      expect.assertions(1)
+      return expect(
+        Promise_.all(expectedPromises.mixedValues)
+      ).resolves.toEqual([2, 4, 6, 1, 3])
+    })
+  })
+})
---
+++ src/explore-es-features/promise/promise.race.test.ts
@@ -0,0 +1,46 @@
+import { resolve, reject } from '../../utils/async'
+import { Promise_ } from './promise'
+
+describe('Promise', () => {
+  describe('promise.race()', () => {
+    it('should process array of resolved promises"', async () => {
+      expect.assertions(1)
+      return expect(
+        Promise_.race([resolve(1, 2), resolve(2, 3), resolve(3, 1)])
+      ).resolves.toBe(3)
+    })
+
+    it('should process array of resolved and rejected promises', async () => {
+      expect.assertions(1)
+      return expect(
+        Promise_.race([reject(4, 2), resolve(5, 3), resolve(6, 1)])
+      ).resolves.toBe(6)
+    })
+
+    it('should process array of resolved and rejected promises', async () => {
+      expect.assertions(1)
+      return expect(
+        Promise_.race([resolve(7, 2), reject(8, 3), resolve(9, 1)])
+      ).resolves.toBe(9)
+    })
+
+    it('should process array of rejected promises', async () => {
+      expect.assertions(1)
+      return expect(
+        Promise_.race([reject(10, 2), reject(11, 3), reject(12, 1)])
+      ).resolves.toBe(12)
+    })
+
+    it('should process array of non-promise values', async () => {
+      expect.assertions(1)
+      return expect(Promise_.race([2, 4, 6])).resolves.toBe(2)
+    })
+
+    it('should process array of non-promise values and promises', async () => {
+      expect.assertions(1)
+      return expect(
+        Promise_.race([2, 4, 6, resolve(1, 1), resolve(3, 2)])
+      ).resolves.toBe(2)
+    })
+  })
+})
