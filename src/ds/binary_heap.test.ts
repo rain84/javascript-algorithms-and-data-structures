877af5150359e0b5d50c493875830b678d74df26
rain84 <rainy_sky@mail.ru>
2022-12-30 03:27:28 +0300

    refactor: :white_check_mark: tests updated

--- src/ds/binary_heap.test.ts
+++ src/ds/binary_heap.test.ts
@@ -1,44 +1,42 @@
 import { random } from '../utils/array'
-import { BinaryHeapMax, BinaryHeapMin } from './binary_heap'
+import { BinaryHeap } from './binary_heap'
 
-describe('Min and max binary heap', () => {
-  interface IHeap {
-    max: MaybeNull<BinaryHeapMax>
-    min: MaybeNull<BinaryHeapMin>
+interface IHeap {
+  max: MaybeNull<BinaryHeap>
+  min: MaybeNull<BinaryHeap>
+}
+let heap: Partial<IHeap> = {}
+let input = [...new Set(random(10, 100))]
+
+beforeEach(() => {
+  heap.max = BinaryHeap.createMax()
+  heap.min = BinaryHeap.createMin()
+  input.forEach((val) => {
+    heap.max?.insert(val)
+    heap.min?.insert(val)
+  })
+})
+
+it('Binary heaps should have "insert()" and "remove()"', () => {
+  type TValues = {
+    max: number[]
+    min: number[]
   }
-  let heap: Partial<IHeap> = {}
-  let input = [...new Set(random(10, 100))]
+  const values: TValues = {
+    max: [],
+    min: [],
+  }
+  let sorted = {
+    asc: [...input],
+    desc: [...input],
+  }
+  let val: number | undefined
 
-  beforeEach(() => {
-    heap.max = new BinaryHeapMax()
-    heap.min = new BinaryHeapMin()
-    input.forEach((val) => {
-      heap.max?.insert(val)
-      heap.min?.insert(val)
-    })
-  })
+  sorted.asc.sort((a, b) => a - b)
+  sorted.desc.sort((a, b) => b - a)
+  while ((val = heap.max?.remove()) !== undefined) values.max.push(val)
+  while ((val = heap.min?.remove()) !== undefined) values.min.push(val)
 
-  it('should have "insert()" and "remove()"', () => {
-    type TValues = {
-      max: number[]
-      min: number[]
-    }
-    const values: TValues = {
-      max: [],
-      min: [],
-    }
-    let sorted = {
-      asc: [...input],
-      desc: [...input],
-    }
-    let val: number | undefined
-
-    sorted.asc.sort((a, b) => a - b)
-    sorted.desc.sort((a, b) => b - a)
-    while ((val = heap.max?.remove()) !== undefined) values.max.push(val)
-    while ((val = heap.min?.remove()) !== undefined) values.min.push(val)
-
-    expect(values.max).toMatchObject(sorted.desc)
-    expect(values.min).toMatchObject(sorted.asc)
-  })
+  expect(values.max).toMatchObject(sorted.desc)
+  expect(values.min).toMatchObject(sorted.asc)
 })
