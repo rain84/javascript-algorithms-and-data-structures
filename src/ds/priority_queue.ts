60ef48d4be5889aac69362d4bd40a85725463b90
rain84 <rainy_sky@mail.ru>
2023-01-04 04:18:32 +0300

    feat: :sparkles: added "fill()" method and support of key selectors

--- src/ds/priority_queue.ts
+++ src/ds/priority_queue.ts
@@ -1,13 +1,13 @@
-import { BinaryHeap, IBinaryHeap } from './binary_heap'
+import { BinaryHeap, IBinaryHeap, type Selector } from './binary_heap'
 
 export class PriorityQueue {
   #heap: BinaryHeap
 
-  static createMin = (values?: number | number[]) =>
-    new PriorityQueue(BinaryHeap.createMin(values))
+  static createMin = (selector?: Selector) =>
+    new PriorityQueue(BinaryHeap.createMin(selector))
 
-  static createMax = (values?: number | number[]) =>
-    new PriorityQueue(BinaryHeap.createMax(values))
+  static createMax = (selector?: Selector) =>
+    new PriorityQueue(BinaryHeap.createMax(selector))
 
   private constructor(heap: IBinaryHeap) {
     this.#heap = heap
@@ -17,11 +17,20 @@ export class PriorityQueue {
     return this.#heap.size
   }
 
+  fill(values: number | number[]) {
+    this.#heap.fill(values)
+    return this
+  }
+
   enqueue(value: number) {
     this.#heap.insert(value)
   }
 
   dequeue() {
     return this.#heap.remove()
+  }
+
+  [Symbol.iterator]() {
+    return this.#heap[Symbol.iterator]()
   }
 }
