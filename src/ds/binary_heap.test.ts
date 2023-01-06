df058e23ff6e5518acd54102840de9031c6e3f7b
rain84 <rainy_sky@mail.ru>
2023-01-06 21:42:25 +0300

    test: :white_check_mark: added tests for the complex data stuctures in the heap

--- src/ds/binary_heap.test.ts
+++ src/ds/binary_heap.test.ts
@@ -1,10 +1,16 @@
 import { random } from '../utils/array'
+import { textToWords } from '../utils/string'
 import { BinaryHeap } from './binary_heap'
 
 type minmax = 'min' | 'max'
 type TValues = {
   max: number[]
   min: number[]
+}
+
+interface IRandomData {
+  key: number
+  val: string
 }
 
 let heap: Record<minmax, MaybeNull<BinaryHeap>> = {
@@ -20,8 +26,10 @@ let input = [...new Set(random(10, 100))]
 beforeEach(() => {
   heap.max = BinaryHeap.createMax().fill(input)
   heap.min = BinaryHeap.createMin().fill(input)
-  sorted.min = [...input.sort((a, b) => a - b)]
-  sorted.max = [...input.sort((a, b) => b - a)]
+  sorted.min = [...input]
+  sorted.max = [...input]
+  sorted.min.sort((a, b) => a - b)
+  sorted.max.sort((a, b) => b - a)
 })
 
 it('Binary heaps should have "insert()" and "remove()"', () => {
@@ -48,3 +56,40 @@ it('should implement [Symbol.iterator]', () => {
   expect([...heap.min!]).toMatchObject(sorted.min!)
   expect([...heap.min!]).toMatchObject(sorted.min!)
 })
+
+it('should work with a complex data structure', () => {
+  const words = textToWords(
+    'Проверяющая  сторона (WebAuthn Relying Party) — web-сервер — хранит открытый ключ, связанный с аккаунтом пользователя, проверяет корректность подписи своих запросов закрытым ключом, хранящимся в аутентификаторе.'
+  )
+  words.length = 10
+  const randomData: IRandomData[] = getRandomComplexData(words)
+
+  const heap = {
+    min: BinaryHeap.createMin<IRandomData>(({ key }) => key),
+    max: BinaryHeap.createMax<IRandomData>(({ key }) => key),
+  }
+  heap.min.fill(randomData)
+  heap.max.fill(randomData)
+
+  expect([...heap.min].map(({ val }) => val)).toMatchObject(words)
+
+  words.reverse()
+  expect([...heap.max].map(({ val }) => val)).toMatchObject(words)
+})
+
+function getRandomComplexData(words: string[]): IRandomData[] {
+  const randomArr = random(10, 10000)
+  const sortedMap = [...randomArr]
+    .sort((a, b) => a - b)
+    .reduce<Record<string, number>>(
+      (acc, val, key) => ((acc[val] = key), acc),
+      {}
+    )
+
+  const randomData = randomArr.map((key) => ({
+    key,
+    val: words[sortedMap[key]],
+  }))
+
+  return randomData
+}
