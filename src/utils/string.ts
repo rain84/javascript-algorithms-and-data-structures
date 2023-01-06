3f664b44e41ed980924c61faabf50e6328c15855
rain84 <rainy_sky@mail.ru>
2023-01-06 18:45:30 +0300

    feat: :sparkles: added "textToWords" util

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,3 +1,5 @@
+import { unicodeDelimiters } from 'regexes'
+
 function stringify(data: any) {
   return JSON.stringify(data, null, ' ')
 }
@@ -6,3 +8,6 @@ export function log(data: any, msg?: string): void {
   const isObject = typeof data === 'object' && !Object.is(data, null)
   console.log(msg, isObject ? stringify(data) : data)
 }
+
+export const textToWords = (str: string): string[] =>
+  str.split(unicodeDelimiters).filter(Boolean)
