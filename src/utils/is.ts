cc9f528b6a8d2075dfd826577240ada58675c125
rain84 <rainy_sky@mail.ru>
2023-01-12 18:21:11 +0300

    feat: :sparkles: added isPOJO util function

---
+++ src/utils/is.ts
@@ -0,0 +1,6 @@
+export const isPOJO = (x: unknown): x is Object => {
+  if (x == null || typeof x !== 'object') return false
+
+  const proto = Object.getPrototypeOf(x)
+  return proto === Object.prototype || proto === null
+}
