49cf27eb5d84b693c8add74667e49c3471d4aed7
rain84 <rainy_sky@mail.ru>
2023-01-12 18:22:13 +0300

    chore: :art: code formatted

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,8 +1,10 @@
 export const random = (length: number, max = length) =>
   new Array(length).fill(0).map(() => ~~(Math.random() * (max + 1)))
+
 export const random2 = (length: number) =>
   Array.apply(null, { length } as Array<unknown>).map(
     Function.call,
     Math.random
   )
+
 export const format = (arr: unknown[]) => `[${arr.join(', ')}]`
