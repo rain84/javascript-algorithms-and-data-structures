c003c7ec52a4b76bed922ae4d027620167464b7d
rain84 <rainy_sky@mail.ru>
2023-03-18 17:27:10 +0300

    feat: :label: added example with a 'const' constraints

---
+++ src/ts-types/consts.ts
@@ -0,0 +1,25 @@
+type HasNames = { names: readonly string[] }
+
+function getNamesExactlyOld<T extends HasNames>(arg: T): T['names'] {
+  return arg.names
+}
+
+function getNamesExactlyNew<const T extends HasNames>(arg: T): T['names'] {
+  return arg.names
+}
+
+//    string[]
+const names1 = getNamesExactlyOld({ names: ['Alice', 'Bob', 'Eve'] })
+
+// Correctly gets what we wanted:
+//    readonly ["Alice", "Bob", "Eve"]
+const names2 = getNamesExactlyNew({ names: ['Alice', 'Bob', 'Eve'] })
+
+
+declare function fnBad<T extends readonly string[]>(args: T): void;
+// 'T' is still 'string[]' since 'readonly ["a", "b", "c"]' is not assignable to 'string[]'
+fnBad(["a", "b", "c"]);
+
+declare function fnGood<const T extends readonly string[]>(args: T): void;
+// T is readonly ["a", "b", "c"]
+fnGood(["a", "b", "c"]);
