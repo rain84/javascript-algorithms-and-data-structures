1512c9e2b243a52a74cf0808359a2dc47a386046
rain84 <rainy_sky@mail.ru>
2023-04-05 15:19:15 +0300

    feat: :label: added TS mapped types

---
+++ src/ts-types/mapped-types.type.ts
@@ -0,0 +1,32 @@
+export type Setters<State> = {
+  [K in keyof State & string as `set${Capitalize<K>}`]: (
+    value: State[K]
+  ) => void
+}
+
+export type Getters<State> = {
+  [K in keyof State & string as `get${Capitalize<K>}`]: () => State[K]
+}
+
+export type Store<State> = Getters<State> & Setters<State>
+
+type PersonState = {
+  name: string
+  // age: number
+}
+
+export type PersonStore = Store<PersonState> & PersonState
+const personStore: PersonStore = {
+  name: '',
+  getName() {
+    return this.name
+  },
+  setName(name) {
+    this.name = name
+  },
+}
+// personStore.setAge(1)
+personStore.setName('Fedor')
+
+const name = personStore.getName()
+console.log(name)
