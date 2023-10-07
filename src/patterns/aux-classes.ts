3957601737e2d84d756f5d5b62627b353f28dd45
rain84 <rainy_sky@mail.ru>
2023-10-08 01:10:38 +0300

    fix: ✏️ fixed typo

--- src/patterns/aux-classes.ts
+++ src/patterns/aux-classes.ts
@@ -1,45 +1,45 @@
-import { EventEmitter } from './event-emmiter'
+import { EventEmitter } from './event-emitter'
 
 export enum EVENT_TYPES {
-	'INCREMENT' = 'INCREMENT',
-	'A:INCREMENT' = 'A:INCREMENT',
-	'DECREMENT' = 'DECREMENT',
-	'ONLY_ONCE' = 'ONLY_ONCE'
+  'INCREMENT' = 'INCREMENT',
+  'A:INCREMENT' = 'A:INCREMENT',
+  'DECREMENT' = 'DECREMENT',
+  'ONLY_ONCE' = 'ONLY_ONCE',
 }
 
 export class ClassA extends EventEmitter {
-	private _counter = 0
+  private _counter = 0
 
-	get counter() {
-		return this._counter
-	}
+  get counter() {
+    return this._counter
+  }
 
-	set counter(val: number) {
-		this._counter = val
-	}
+  set counter(val: number) {
+    this._counter = val
+  }
 
-	constructor() {
-		super()
+  constructor() {
+    super()
 
-		this.on(EVENT_TYPES.INCREMENT, () => this._counter++)
-		this.on(EVENT_TYPES['A:INCREMENT'], () => this._counter++)
-		this.on(EVENT_TYPES.DECREMENT, () => this._counter--)
-		this.once(EVENT_TYPES.ONLY_ONCE, () => (this._counter += 100))
-	}
+    this.on(EVENT_TYPES.INCREMENT, () => this._counter++)
+    this.on(EVENT_TYPES['A:INCREMENT'], () => this._counter++)
+    this.on(EVENT_TYPES.DECREMENT, () => this._counter--)
+    this.once(EVENT_TYPES.ONLY_ONCE, () => (this._counter += 100))
+  }
 }
 
 export class ClassB extends EventEmitter {
-	private _counter = 0
+  private _counter = 0
 
-	get counter() {
-		return this._counter
-	}
-	constructor() {
-		super()
+  get counter() {
+    return this._counter
+  }
+  constructor() {
+    super()
 
-		this.on(EVENT_TYPES.INCREMENT, () => (this._counter += 2))
-		this.on(EVENT_TYPES['A:INCREMENT'], () => this._counter++)
-		this.on(EVENT_TYPES.DECREMENT, () => (this._counter -= 2))
-		this.once(EVENT_TYPES.ONLY_ONCE, () => (this._counter += 200))
-	}
+    this.on(EVENT_TYPES.INCREMENT, () => (this._counter += 2))
+    this.on(EVENT_TYPES['A:INCREMENT'], () => this._counter++)
+    this.on(EVENT_TYPES.DECREMENT, () => (this._counter -= 2))
+    this.once(EVENT_TYPES.ONLY_ONCE, () => (this._counter += 200))
+  }
 }
