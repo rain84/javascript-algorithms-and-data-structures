5fbee59c32c34d9f5f8c4eb5627ff78fc063dae2
rain84 <rainy_sky@mail.ru>
2024-04-21 13:52:07 +0300

    feat(LeetCode DSA): ✨ added numSubarrayProductLessThanK

---
+++ src/leetcode/dsa/arrays_and_strings/subarrayProductLessThanK.ts
@@ -0,0 +1,44 @@
+// https://leetcode.com/problems/subarray-product-less-than-k/description/
+
+export function numSubarrayProductLessThanK(arr: number[], k: number): number {
+  if (k <= 1) return 0
+
+  let r = 0,
+    l = 0
+  let product = 1
+  let res = 0
+
+  for (let r = 0; r < arr.length; r++) {
+    product *= arr[r]
+
+    while (product >= k) product /= arr[l++]
+
+    res += r - l + 1
+  }
+
+  return res
+}
+
+export const subarrayProductLessThanK = (arr: number[], maxProduct: number): number[][] => {
+  const res: number[][] = []
+
+  for (let size = 1; size <= arr.length; size++) {
+    let l = 0
+
+    for (let l = 0; l + size <= arr.length; l++) {
+      let product = 1
+      const nextArr: number[] = []
+      for (let l2 = l; l2 < l + size; l2++) {
+        product *= arr[l2]
+        nextArr.push(arr[l2])
+      }
+
+      if (product < maxProduct) {
+        product
+        res.push(nextArr)
+      }
+    }
+  }
+
+  return res
+}
