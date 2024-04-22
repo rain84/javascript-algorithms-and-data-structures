bd0f1ae5430cc03d8148b1ffe93a4fc1e56587e1
rain84 <rainy_sky@mail.ru>
2024-04-22 22:23:13 +0300

    feat(LeetCode DSA): ✨ added minStartValue

---
+++ src/leetcode/dsa/arrays_and_strings/minStartValue.ts
@@ -0,0 +1,13 @@
+//  https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/703/arraystrings/4657/
+
+export const minStartValue = (nums: number[]): number => {
+  let result = 1
+  let sum = 0
+
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i]
+    result = Math.max(result, 1 - sum)
+  }
+
+  return result
+}
