96bcfe6b0aa6d7589865f0570be630b7771e8cd8
rain84 <rainy_sky@mail.ru>
2024-04-23 19:47:07 +0300

    feat(LeetCode DSA): ✨ added getAverages

---
+++ src/leetcode/dsa/arrays_and_strings/getAverages.test.ts
@@ -0,0 +1,15 @@
+import { getAverages } from './getAverages'
+
+it('should work 1', () => {
+  expect(getAverages([7, 4, 3, 9, 1, 8, 5, 2, 6], 3)).toMatchObject([
+    -1, -1, -1, 5, 4, 4, -1, -1, -1,
+  ])
+})
+
+it('should work 2', () => {
+  expect(getAverages([100000], 0)).toMatchObject([100000])
+})
+
+it('should work 3', () => {
+  expect(getAverages([8], 100000)).toMatchObject([-1])
+})
---
+++ src/leetcode/dsa/arrays_and_strings/getAverages.ts
@@ -0,0 +1,23 @@
+// https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/703/arraystrings/4836/
+
+export const getAverages = (nums: number[], k: number): number[] => {
+  const res: number[] = []
+  const prefixes: number[] = [nums[0]]
+
+  for (let index = 1; index < nums.length; index++) {
+    const element = nums[index]
+    prefixes.push(element + prefixes[index - 1])
+  }
+
+  for (let index = 0; index < nums.length; index++) {
+    const outOfRadius = index - k < 0 || index + k >= nums.length
+    if (outOfRadius) res.push(-1)
+    else {
+      const sum = prefixes[index + k] - prefixes[index - k] + nums[index - k]
+      const n = 2 * k + 1
+      res.push((sum / n) | 0)
+    }
+  }
+
+  return res
+}
