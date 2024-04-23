6b5ef201996d12cec0412b4a9a6efdfd37b7454a
rain84 <rainy_sky@mail.ru>
2024-04-23 19:47:29 +0300

    feat(LeetCode DSA): ✨ added reverseWords

---
+++ src/leetcode/dsa/arrays_and_strings/reverseWords.ts
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/reverse-words-in-a-string-iii/submissions/1240042260/
+
+export function reverseWords(s: string): string {
+  const res: string[] = []
+  const SPACE = ' '
+
+  for (let i = 0, l = 0; i < s.length; i++) {
+    const ch = s[i]
+    const next = s[i + 1]
+    const prev = s[i - 1]
+
+    if (ch === SPACE) {
+      res.push(ch)
+      continue
+    }
+
+    if (prev === SPACE) l = i
+    if (next && next !== SPACE) continue
+
+    let r = i
+
+    while (r >= l) {
+      res.push(s[r])
+      r--
+    }
+  }
+
+  return res.join('')
+}
