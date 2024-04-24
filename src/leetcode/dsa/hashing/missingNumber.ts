37e889a28648b1aecc27fb0f19cc3b9e10230061
rain84 <rainy_sky@mail.ru>
2024-04-25 01:28:49 +0300

    feat(LeetCode DSA): ✨ added missingNumber

---
+++ src/leetcode/dsa/hashing/missingNumber.ts
@@ -0,0 +1,12 @@
+// https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/705/hashing/4602/
+
+export function missingNumber(nums: number[]): number {
+  const max = Math.max(...nums)
+  const set = new Set(nums)
+
+  for (let i = 0; i < max; i++) {
+    if (!set.has(i)) return i
+  }
+
+  return max + 1
+}
