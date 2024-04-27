41ed9805fab98681b281e09de523c23e5c322ad6
rain84 <rainy_sky@mail.ru>
2024-04-27 15:12:28 +0300

    feat(LeetCode DSA): ✨ added findMaxLength

---
+++ src/leetcode/dsa/hashing/findMaxLength.ts
@@ -0,0 +1,20 @@
+// https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/705/hashing/4845/
+
+export function findMaxLength(nums: number[]): number {
+  let cx = new Map([[0, -1]])
+  let c = 0
+  let max = 0
+
+  for (let i = 0; i < nums.length; i++) {
+    const val = nums[i]
+
+    if (val) c++
+    else c--
+
+    const idx = cx.get(c)
+    if (idx === undefined) cx.set(c, i)
+    else max = Math.max(max, i - idx)
+  }
+
+  return max
+}
