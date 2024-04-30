333d19b345fd648f730525f2a953fccaed413d51
rain84 <rainy_sky@mail.ru>
2024-04-30 21:27:50 +0300

    feat(LeetCode DSA): ✨ added lengthOfLongestSubstring

---
+++ src/leetcode/dsa/hashing/lengthOfLongestSubstring.ts
@@ -0,0 +1,29 @@
+// https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/705/hashing/4690/
+
+export function lengthOfLongestSubstring(s: string): number {
+  let counter = 0
+  let max = 0
+  let l = 0
+  let r = 0
+  let obj: Record<string, number> = {}
+
+  while (r < s.length) {
+    const chRight = s[r]
+    obj[chRight] = (obj[chRight] ?? 0) + 1
+
+    while (obj[chRight] === 2) {
+      const chLeft = s[l]
+
+      obj[chLeft]--
+      counter--
+      l++
+    }
+
+    counter++
+    r++
+
+    max = Math.max(max, counter)
+  }
+
+  return max
+}
