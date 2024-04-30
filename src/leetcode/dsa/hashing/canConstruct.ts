522573fcd8cd6ecc86930ef0ea61c558a6ad65b8
rain84 <rainy_sky@mail.ru>
2024-04-30 20:31:28 +0300

    feat(LeetCode DSA): ✨ added canConstruct

---
+++ src/leetcode/dsa/hashing/canConstruct.ts
@@ -0,0 +1,19 @@
+//  https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/705/hashing/4607/
+
+export function canConstruct(ransomNote: string, magazine: string): boolean {
+  const o: Record<string, number> = {}
+  let c = 0
+
+  for (const ch of ransomNote) {
+    o[ch] = (o[ch] ?? 0) + 1
+    c++
+  }
+
+  for (const ch of magazine) {
+    if (o[ch] === undefined) continue
+    if (--o[ch] >= 0) c--
+    if (c === 0) return true
+  }
+
+  return false
+}
