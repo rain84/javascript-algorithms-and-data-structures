348fb07a8591df4d530b9e6192f15c753187bd3d
rain84 <rainy_sky@mail.ru>
2024-04-30 19:54:21 +0300

    chore(LeetCode DSA): 📝 added link to task description

--- src/leetcode/dsa/hashing/equalPairs.ts
+++ src/leetcode/dsa/hashing/equalPairs.ts
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/equal-row-and-column-pairs/
+
 export function equalPairs(grid: number[][]): number {
   let res = 0
   const rows = new Map<string, number>()
@@ -19,37 +21,3 @@ export function equalPairs(grid: number[][]): number {
 
   return res
 }
-
-const input = [
-  [3, 2, 1],
-  [1, 7, 6],
-  [2, 7, 7],
-]
-equalPairs(input)
-
-/* export function equalPairs(grid: number[][]): number {
-  let res = 0
-
-  const isEqual = (a1: number[], a2: number[]) => a1.every((val, i) => val === a2[i])
-
-  const gridCol = Array(grid.length)
-    .fill(0)
-    .map<number[]>(() => [])
-
-  for (const a1 of grid) {
-    for (let i = 0; i < a1.length; i++) {
-      gridCol[i].push(a1[i])
-    }
-  }
-
-  for (const a1 of grid) {
-    for (const a2 of gridCol) {
-      if (isEqual(a1, a2)) {
-        res++
-      }
-    }
-  }
-
-  return res
-}
- */
