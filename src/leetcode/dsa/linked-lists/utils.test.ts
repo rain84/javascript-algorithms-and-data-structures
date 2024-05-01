59ea8065cb14538996d4f8b276336671bf0afc02
rain84 <rainy_sky@mail.ru>
2024-05-01 17:51:16 +0300

    feat(LeetCode DSA): ✅ added tests for linked list utils

---
+++ src/leetcode/dsa/linked-lists/utils.test.ts
@@ -0,0 +1,25 @@
+import { ListNode, createLinkedList, toArray } from './utils'
+
+export { ListNode, createLinkedList, toArray } from './utils'
+
+it('ListNode should work', () => {
+  const node = new ListNode(1, new ListNode(2))
+
+  expect(node.val).toBe(1)
+  expect(node.next!.val).toBe(2)
+  expect(node.next!.next).toBeNull()
+})
+
+it('createLinkedList should work', () => {
+  const node = createLinkedList([1, 3])
+
+  expect(node.val).toBe(1)
+  expect(node.next!.val).toBe(3)
+  expect(node.next!.next).toBeNull()
+})
+
+it('toArray should work', () => {
+  const arr = toArray(createLinkedList([1, 2, 3]))
+
+  expect(arr).toMatchObject([1, 2, 3])
+})
