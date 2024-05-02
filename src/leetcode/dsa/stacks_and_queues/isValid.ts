0677ed2d461b2feff53c2d78a9ee67141af27779
rain84 <rainy_sky@mail.ru>
2024-05-02 23:37:14 +0300

    feat(LeetCode DSA): ✨ added isValid

---
+++ src/leetcode/dsa/stacks_and_queues/isValid.ts
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/valid-parentheses/
+
+export function isValid(s: string): boolean {
+  const stack = []
+  const pairs: Record<string, string> = {
+    '(': ')',
+    '{': '}',
+    '[': ']',
+  }
+
+  for (const ch of s) {
+    if (pairs[ch]) {
+      stack.push(pairs[ch])
+    } else if (stack.pop() !== ch) {
+      return false
+    }
+  }
+
+  return stack.length === 0
+}
