3957fdf41a741393bb4fd8345113b8483e41f9e8
rain84 <rainy_sky@mail.ru>
2024-05-02 23:54:23 +0300

    feat(LeetCode DSA): ✨ added removeDuplicates

---
+++ src/leetcode/dsa/stacks_and_queues/removeDuplicates.ts
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+
+export function removeDuplicates(s: string): string {
+  const stack: string[] = []
+
+  for (const ch of s) {
+    if (ch === stack.at(-1)) {
+      stack.pop()
+    } else stack.push(ch)
+  }
+
+  return stack.join('')
+}
