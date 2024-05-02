27650fe8c7973bda4fd703d4e0213d53e3c750d9
rain84 <rainy_sky@mail.ru>
2024-05-02 19:47:28 +0300

    feat(LeetCode DSA): ✨ added pairSum

---
+++ src/leetcode/dsa/linked-lists/pairSum.ts
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/
+
+import { ListNode } from './utils'
+
+export function pairSum(head: ListNode | null): number {
+  if (!head?.next) return 0
+
+  let fast: ListNode | null = head
+  let slow: ListNode | null = head
+  let reversed = null
+
+  while (slow && fast?.next) {
+    fast = fast.next.next
+    const next: ListNode | null = slow.next
+    slow.next = reversed
+    reversed = slow
+    slow = next
+  }
+
+  let max = 0
+  while (reversed) {
+    max = Math.max(max, slow!.val + reversed!.val)
+    slow = slow!.next
+    reversed = reversed.next
+  }
+
+  return max
+}
