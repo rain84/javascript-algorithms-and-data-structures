48c48429c329693540e358c04d355a8c043b66e1
rain84 <rainy_sky@mail.ru>
2024-05-02 22:07:08 +0300

    feat(LeetCode DSA): 🚧 added reverseBetween (WIP)

---
+++ src/leetcode/dsa/linked-lists/reverseBetween.WIP.ts
@@ -0,0 +1,33 @@
+import { ListNode } from './utils'
+
+// TODO: add handling for the boundary inputs
+export function reverseBetween(
+  head: ListNode | null,
+  left: number,
+  right: number
+): ListNode | null {
+  let node = head
+  let reversed: ListNode | null = null
+  let listLeft: ListNode | null = null
+  let reverseLast: ListNode | null = null
+  let c = 1
+
+  while (node && c <= right) {
+    const next = node.next
+
+    if (c === left - 1) listLeft = node
+    if (c >= left) {
+      node.next = reversed
+      reversed = node
+      if (c === left) reverseLast = reversed
+    }
+
+    node = next
+    c++
+  }
+
+  listLeft!.next = reversed
+  reverseLast!.next = node
+
+  return head
+}
