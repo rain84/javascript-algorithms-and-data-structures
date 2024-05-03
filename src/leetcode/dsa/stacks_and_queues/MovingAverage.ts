273e1101e7d966ff11e899c23beab69bb112e8b1
rain84 <rainy_sky@mail.ru>
2024-05-04 00:58:25 +0300

    feat(LeetCode DSA): ✨ added MovingAverage

---
+++ src/leetcode/dsa/stacks_and_queues/MovingAverage.ts
@@ -0,0 +1,18 @@
+// https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/706/stacks-and-queues/4703/
+
+export class MovingAverage {
+  #queue: number[] = []
+  #sum = 0
+
+  constructor(private size: number) {}
+
+  next(val: number): number {
+    if (this.#queue.length === this.size) this.#sum -= this.#queue.shift()!
+
+    this.#queue.push(val)
+    this.#sum += val
+    const average = this.#sum / this.#queue.length
+
+    return +average.toFixed(5)
+  }
+}
