cd3728b6cf03a6fa6082cabed0df038cba37e1f0
rain84 <rainy_sky@mail.ru>
2024-05-04 10:33:26 +0300

    feat(LeetCode DSA): ✨ added dailyTemperatures

---
+++ src/leetcode/dsa/stacks_and_queues/dailyTemperatures.test.ts
@@ -0,0 +1,22 @@
+import { dailyTemperatures } from './dailyTemperatures'
+
+it('should work 1', () => {
+  const input = [73, 74, 75, 71, 69, 72, 76, 73]
+  const output = [1, 1, 4, 2, 1, 1, 0, 0]
+
+  expect(dailyTemperatures(input)).toMatchObject(output)
+})
+
+it('should work 2', () => {
+  const input = [30, 40, 50, 60]
+  const output = [1, 1, 1, 0]
+
+  expect(dailyTemperatures(input)).toMatchObject(output)
+})
+
+it('should work 3', () => {
+  const input = [30, 60, 90]
+  const output = [1, 1, 0]
+
+  expect(dailyTemperatures(input)).toMatchObject(output)
+})
---
+++ src/leetcode/dsa/stacks_and_queues/dailyTemperatures.ts
@@ -0,0 +1,17 @@
+//  https://leetcode.com/problems/daily-temperatures
+
+export function dailyTemperatures(temperatures: number[]): number[] {
+  const stack: number[] = [0]
+  const res = Array(temperatures.length).fill(0)
+
+  for (let i = 1; i < temperatures.length; i++) {
+    while (stack.length && temperatures[stack.at(-1)!] < temperatures[i]) {
+      let j = stack.pop()!
+      res[j!] = i - j
+    }
+
+    stack.push(i)
+  }
+
+  return res
+}
