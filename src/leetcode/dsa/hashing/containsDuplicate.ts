e03b03445976ba6cc11c687000b20c6c89399c0c
rain84 <rainy_sky@mail.ru>
2024-05-06 23:44:59 +0300

    feat(LeetCode DSA): ✨ added containsDuplicate

---
+++ src/leetcode/dsa/hashing/containsDuplicate.ts
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/contains-duplicate
+
+export function containsDuplicate(nums: number[]): boolean {
+  const hashes: Record<string, boolean> = {}
+
+  for (const x of nums) {
+    if (hashes[x]) return true
+    hashes[x] = true
+  }
+
+  return false
+}
