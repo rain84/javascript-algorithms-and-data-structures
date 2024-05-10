ec38eae574f840483fe68c35c30a25adf14d1762
rain84 <rainy_sky@mail.ru>
2024-05-11 01:07:52 +0300

    feat(LeetCode DSA): ✨ added finalPrices

---
+++ src/leetcode/dsa/stacks_and_queues/finalPrices.test.ts
@@ -0,0 +1,17 @@
+import { finalPrices } from './finalPrices'
+
+it('should work 1', () => {
+  expect(finalPrices([8, 4, 6, 2, 3])).toMatchObject([4, 2, 4, 2, 3])
+})
+
+it('should work 2', () => {
+  expect(finalPrices([1, 2, 3, 4, 5])).toMatchObject([1, 2, 3, 4, 5])
+})
+
+it('should work 3', () => {
+  expect(finalPrices([10, 1, 1, 6])).toMatchObject([9, 0, 1, 6])
+})
+
+it('should work 4', () => {
+  expect(finalPrices([8, 7, 4, 2, 8, 1, 7, 7, 10, 1])).toMatchObject([1, 3, 2, 1, 7, 0, 0, 6, 9, 1])
+}) //                [1, 3, 2, 1, 7, 0, 0, 6,  9, 1]
---
+++ src/leetcode/dsa/stacks_and_queues/finalPrices.ts
@@ -0,0 +1,47 @@
+/**
+ * Description {@link https://leetcode.com/problems/find-the-most-competitive-subsequence/}
+ * Time complexity: O(n)
+ * Space complexity: O(n)
+ */
+export function finalPrices(prices: number[]): number[] {
+  const res = Array(prices.length).fill(0)
+  const stack: number[] = []
+
+  for (let i = prices.length - 1; i >= 0; i--) {
+    const x = prices[i]
+
+    while (stack.length && stack.at(-1)! > x) {
+      stack.pop()
+    }
+
+    const discount = stack.at(-1)! ?? 0
+    res[i] = x - discount
+
+    stack.push(x)
+  }
+
+  return res
+}
+
+/**
+ * Brute-force solution
+ * Time complexity: O(n^2)
+ */
+function finalPrices2(prices: number[]): number[] {
+  const res: number[] = []
+
+  for (let i = 0; i < prices.length; i++) {
+    const x = prices[i]
+
+    let discount = 0
+    for (let j = i + 1; j < prices.length; j++) {
+      if (prices[j] <= prices[i]) {
+        discount = prices[j]
+        break
+      }
+    }
+
+    res.push(x - discount)
+  }
+  return res
+}
