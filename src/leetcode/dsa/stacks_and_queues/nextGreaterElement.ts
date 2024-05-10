82a9e6746593cad82302a24e7254fe4f013a5704
rain84 <rainy_sky@mail.ru>
2024-05-10 17:11:48 +0300

    feat(LeetCode DSA): ✨ added nextGreaterElement

---
+++ src/leetcode/dsa/stacks_and_queues/nextGreaterElement.ts
@@ -0,0 +1,25 @@
+// https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/706/stacks-and-queues/4612/
+
+export function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
+  const res: number[] = []
+  const map: Record<number, number> = {}
+  const stack: number[] = [nums2[0]]
+
+  for (let i = 1; i < nums2.length; i++) {
+    const nextGE = nums2[i]
+
+    while (nextGE > stack.at(-1)!) {
+      const x = stack.pop()!
+      map[x] = nextGE
+    }
+    stack.push(nextGE)
+  }
+
+  while (stack.length) {
+    map[stack.pop()!] = -1
+  }
+
+  for (const i of nums1) res.push(map[i])
+
+  return res
+}
