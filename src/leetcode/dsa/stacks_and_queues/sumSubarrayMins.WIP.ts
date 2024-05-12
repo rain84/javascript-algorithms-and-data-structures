dbc5e7805e59274c7bb9d8c39048459be9161e42
rain84 <rainy_sky@mail.ru>
2024-05-12 17:22:43 +0300

    feat(LeetCode DSA): 🚧 added sumSubarrayMins (WIP)

---
+++ src/leetcode/dsa/stacks_and_queues/sumSubarrayMins.WIP.ts
@@ -0,0 +1,23 @@
+/**
+ * Description {@link https://leetcode.com/problems/sum-of-subarray-minimums}
+ *
+ * WIP, too long execution
+ * Description of the solution can be checked here {@link https://doocs.github.io/leetcode/en/lc/907/}
+ */
+export function sumSubarrayMins(arr: number[]): number {
+  let sum = 0
+
+  for (let i = 0; i < arr.length; i++) {
+    const stack: number[] = []
+    for (let j = i; j < arr.length; j++) {
+      const x = arr[j]
+
+      while (stack.length && stack.at(-1)! > x) stack.pop()
+      stack.push(x)
+
+      sum += stack[0]
+    }
+  }
+
+  return sum % (1e9 + 7)
+}
