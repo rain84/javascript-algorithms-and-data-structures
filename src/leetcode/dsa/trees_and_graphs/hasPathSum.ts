0b5fcb4a336119d6ab03d873baa75905e44bbad9
rain84 <rainy_sky@mail.ru>
2024-05-13 00:26:24 +0300

    feat(LeetCode DSA): ✨ added hasPathSum

---
+++ src/leetcode/dsa/trees_and_graphs/hasPathSum.ts
@@ -0,0 +1,11 @@
+import type { TreeNode } from '../utils'
+
+export function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
+  if (!root) return false
+
+  const diff = targetSum - root.val
+
+  if (diff === 0 && !root.left && !root.right) return true
+
+  return hasPathSum(root.left, diff) || hasPathSum(root.right, diff)
+}
