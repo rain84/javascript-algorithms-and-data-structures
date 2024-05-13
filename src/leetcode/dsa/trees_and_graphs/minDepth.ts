5e2ffea166e67f9a9735143ce79bb043bb371456
rain84 <rainy_sky@mail.ru>
2024-05-14 00:00:16 +0300

    feat(LeetCode DSA): ✨ added minDepth

---
+++ src/leetcode/dsa/trees_and_graphs/minDepth.ts
@@ -0,0 +1,14 @@
+import type { TreeNode } from '../utils'
+/**
+ *
+ * @description {@link https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/707/traversals-trees-graphs/4691/}
+ *
+ */
+export function minDepth(root: TreeNode | null): number {
+  if (!root) return 0
+
+  const l = minDepth(root.left)
+  const r = minDepth(root.right)
+
+  return 1 + (l && r ? Math.min(l, r) : l || r)
+}
