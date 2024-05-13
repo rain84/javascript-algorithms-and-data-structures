ae3af76358a1b07f7f1c2bc7a1ea13ac0490f0df
rain84 <rainy_sky@mail.ru>
2024-05-14 02:41:06 +0300

    feat(LeetCode DSA): ✨ added maxAncestorDiff

---
+++ src/leetcode/dsa/trees_and_graphs/maxAncestorDiff.ts
@@ -0,0 +1,26 @@
+import type { TreeNode } from '../utils'
+/**
+ * @description https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/707/traversals-trees-graphs/4617/
+ *
+ */
+export function maxAncestorDiff(root: T): number {
+  if (!root) return 0
+
+  const dfs = (node: T, max = Number.NEGATIVE_INFINITY, min = Number.POSITIVE_INFINITY): number => {
+    if (!node) return Math.abs(max - min)
+
+    max = Math.max(node.val, max)
+    min = Math.min(node.val, min)
+
+    const left = dfs(node.left, max, min)
+    const right = dfs(node.right, max, min)
+
+    return Math.max(left, right)
+  }
+
+  return dfs(root)
+}
+/**
+ *
+ */
+export type T = TreeNode | null
