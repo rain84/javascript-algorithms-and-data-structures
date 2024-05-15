b14939296c939f5475b49c48453207222298966f
rain84 <rainy_sky@mail.ru>
2024-05-15 16:42:59 +0300

    feat(LeetCode DSA): ✨ added diameterOfBinaryTree

---
+++ src/leetcode/dsa/trees_and_graphs/diameterOfBinaryTree.ts
@@ -0,0 +1,24 @@
+import { type TreeNode } from '../utils'
+
+/**
+ * @description https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/707/traversals-trees-graphs/4669/
+ */
+export function diameterOfBinaryTree(root: T): number {
+  let path = 0
+
+  const dfs = (node: T): number => {
+    if (!node) return 0
+
+    const left = dfs(node.left)
+    const right = dfs(node.right)
+    path = Math.max(path, left + right)
+
+    return Math.max(left, right) + 1
+  }
+
+  dfs(root)
+
+  return path
+}
+
+type T = TreeNode | null
