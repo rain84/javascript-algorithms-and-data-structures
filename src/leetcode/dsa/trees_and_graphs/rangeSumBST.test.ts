f7560c7160d9cad27faa481443c2886d6c5e2b9e
rain84 <rainy_sky@mail.ru>
2024-05-16 23:42:24 +0300

    feat(LeetCode DSA): ✨ added rangeSumBST

---
+++ src/leetcode/dsa/trees_and_graphs/rangeSumBST.test.ts
@@ -0,0 +1,42 @@
+import { createTree } from '../utils'
+import { rangeSumBST } from './rangeSumBST'
+
+it('should work 1', () => {
+  const tree = createTree('10 [5 3 7] [15 null 18]')
+  expect(rangeSumBST(tree, 7, 15)).toBe(32)
+})
+
+it('should work 2', () => {
+  const tree = createTree('10 [5 [3 1] [7 6]] [15 13 18]')
+  expect(rangeSumBST(tree, 6, 10)).toBe(23)
+})
+
+it('should work 3', () => {
+  // [15,9,21,7,13,19,23,5,null,11,null,17]
+  //                 15
+  //       9                 21
+  //   7       13        19      23
+  // 5 null  11 null   17
+  const tree = createTree('15 [ 9 [7 5] [13 11]]   [21 [19 17] 23]')
+  expect(rangeSumBST(tree, 19, 21)).toBe(40)
+})
+
+it('should work 4', () => {
+  // [15,9,21,7,13,19,23,5,null,11,null,17]
+  //                     15
+  //         9                       21
+  //   7           13            19      23
+  // 5 null      11 null       17
+  const tree = createTree('15 [ 9 [7 5] [13 11]]   [21 [19 17] 23]')
+  expect(rangeSumBST(tree, 21, 23)).toBe(44)
+})
+
+it('should work 5', () => {
+  // [18,9,27,6,15,24,30,3,null,12,null,21]
+  //                   18
+  //         9                  27
+  //   6         15          24        30
+  // 3 null    12 null    21
+  const tree = createTree('18 [9 [6 3] [15 12]] [27 [24 21] 30]')
+  expect(rangeSumBST(tree, 18, 24)).toBe(63)
+})
---
+++ src/leetcode/dsa/trees_and_graphs/rangeSumBST.ts
@@ -0,0 +1,19 @@
+import { TreeNode } from '../utils'
+
+/**
+ *
+ * @description https://leetcode.com/problems/range-sum-of-bst/
+ *
+ */
+export function rangeSumBST(root: TreeNode | null, low: number, high: number): number {
+  if (!root) return 0
+
+  const { left, right, val } = root
+  let sum = 0
+
+  if (low < val) sum += rangeSumBST(left, low, high)
+  if (low <= val && val <= high) sum += val
+  if (val < high) sum += rangeSumBST(right, low, high)
+
+  return sum
+}
