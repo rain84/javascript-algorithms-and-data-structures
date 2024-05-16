b8f82e956fb19dd748771feb1358ad9f638a3416
rain84 <rainy_sky@mail.ru>
2024-05-16 09:59:40 +0300

    feat(LeetCode DSA): ✨ added deepestLeavesSum

---
+++ src/leetcode/dsa/trees_and_graphs/deepestLeavesSum.ts
@@ -0,0 +1,30 @@
+import { TreeNode } from '../utils'
+
+/**
+ *
+ * @description https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/707/traversals-trees-graphs/4620/
+ *
+ */
+export function deepestLeavesSum(root: TreeNode | null): number {
+  if (!root) return 0
+
+  let queue = [root]
+  let xs: number[] = []
+
+  while (queue.length) {
+    const nextQueue: TreeNode[] = []
+    xs = []
+
+    while (queue.length) {
+      const { val, left, right } = queue.pop()!
+      xs.push(val)
+
+      if (left) nextQueue.push(left)
+      if (right) nextQueue.push(right)
+    }
+
+    queue = nextQueue
+  }
+
+  return xs.reduce((a, b) => a + b)
+}
