5554222d8cd42871412a0c6201eb673f0ff01408
rain84 <rainy_sky@mail.ru>
2024-05-16 08:16:35 +0300

    feat(LeetCode DSA): ✨ added rightSideView

---
+++ src/leetcode/dsa/trees_and_graphs/rightSideView.ts
@@ -0,0 +1,28 @@
+import { isDefined } from 'typescript/utils'
+import { type TreeNode } from '../utils'
+
+/**
+ * @description https://leetcode.com/problems/binary-tree-right-side-view/description/
+ *
+ */
+export function rightSideView(root: TreeNode | null): number[] {
+  if (!root) return []
+
+  let queue = [root]
+  const res: number[] = []
+
+  while (queue.length) {
+    const nextQueue: TreeNode[] = []
+    res.push(queue.at(-1)!.val)
+
+    for (const node of queue) {
+      isDefined(node)
+      if (node.left) nextQueue.push(node.left)
+      if (node.right) nextQueue.push(node.right)
+    }
+
+    queue = nextQueue
+  }
+
+  return res
+}
