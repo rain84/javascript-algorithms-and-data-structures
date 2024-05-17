04a00d08afd9f90535c0ce918ceea0dad9be1a07
rain84 <rainy_sky@mail.ru>
2024-05-17 23:54:19 +0300

    feat(LeetCode DSA): ✨ isValidBST

---
+++ src/leetcode/dsa/trees_and_graphs/isValidBST.ts
@@ -0,0 +1,24 @@
+import type { TreeNode } from '../utils'
+
+/**
+ * 98. Validate Binary Search Tree
+ * URL {@link https://leetcode.com/problems/validate-binary-search-tree/description/}
+ *
+ */
+export function isValidBST(root: TreeNode | null): boolean {
+  if (!root) return true
+
+  let prev: TreeNode | null = null
+
+  const dfs = (node: TreeNode | null): boolean => {
+    if (!node) return true
+
+    if (!dfs(node.left)) return false
+    if (prev && prev.val >= node.val) return false
+    prev = node
+
+    return dfs(node.right)
+  }
+
+  return dfs(root)
+}
