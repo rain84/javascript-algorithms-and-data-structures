8e343d00d4f3b600fb9aa27a82760f70a9ed7b57
rain84 <rainy_sky@mail.ru>
2024-05-17 19:16:43 +0300

    feat(LeetCode DSA): ✨ add getMinimumDifference

--- src/leetcode/dsa/trees_and_graphs/getMinimumDifference.ts
+++ src/leetcode/dsa/trees_and_graphs/getMinimumDifference.ts
@@ -1,11 +1,31 @@
 import { TreeNode } from '../utils'
 
 /**
- *
- * @description https://leetcode.com/problems/minimum-absolute-difference-in-bst/
+ * 530. Minimum Absolute Difference in BST
+ * URL {@link https://leetcode.com/problems/minimum-absolute-difference-in-bst/}
  *
  */
 export function getMinimumDifference(root: TreeNode | null): number {
+  if (!root) return 0
+
+  let prev = Number.MIN_SAFE_INTEGER
+  let min = Number.MAX_SAFE_INTEGER
+
+  const dfs = (node: TreeNode | null) => {
+    if (!node) return
+
+    dfs(node.left)
+    min = Math.min(min, node.val - prev)
+    prev = node.val
+    dfs(node.right)
+  }
+
+  dfs(root)
+
+  return min
+}
+
+export function getMinimumDifference2(root: TreeNode | null): number {
   if (!root) return 0
 
   const xs: number[] = []
