b45770b0df7e4592bf8ff24d48d30c1c9eb54ca5
rain84 <rainy_sky@mail.ru>
2024-05-19 03:43:46 +0300

    feat(LeetCode DSA): ✨ add TimeLimitedCache (leetcode problem № 2622)

--- src/leetcode/30-Days-of-JavaScript.test.ts
+++ src/leetcode/30-Days-of-JavaScript.test.ts
@@ -1,4 +1,5 @@
-import { compose, once, timeLimit, type F } from './30-Days-of-JavaScript'
+import { delay } from 'utils/promise'
+import { TimeLimitedCache, compose, once, timeLimit, type F } from './30-Days-of-JavaScript'
 
 it('compose() should work', () => {
   const fx: F[] = [(x) => x + 1, (x) => x * x, (x) => 2 * x]
@@ -41,4 +42,21 @@ it('timeLimit should work 4', async () => {
     throw 'Error'
   }
   await expect(timeLimit(fn, 1000)([])).rejects.toBe('Error')
+})
+
+it('TimeLimitedCache should work', async () => {
+  const cache = new TimeLimitedCache()
+  expect(cache.set(1, 42, 100)).toBe(false)
+
+  await delay(50)
+  expect(cache.get(1)).toBe(42)
+
+  await delay(25)
+  expect(cache.count()).toBe(1)
+
+  await delay(25)
+  expect(cache.count()).toBe(0)
+
+  await delay(150)
+  expect(cache.get(1)).toBe(-1)
 })
--- src/leetcode/30-Days-of-JavaScript.ts
+++ src/leetcode/30-Days-of-JavaScript.ts
@@ -61,6 +61,37 @@ const timeLimit2 =
     }
   }
 
+/**
+ * 2622. Cache With Time Limit
+ * URL {@link https://leetcode.com/problems/cache-with-time-limit/description/}
+ */
+export class TimeLimitedCache {
+  #cache = new Map<number, [value: number, expire: number]>()
+
+  set(key: number, value: number, duration: number): boolean {
+    const isExist = this.#cache.has(key)
+
+    if (!this.#isExpired(key)) {
+      this.#cache.set(key, [value, Date.now() + duration])
+    }
+
+    return isExist
+  }
+
+  get(key: number): number {
+    if (this.#isExpired(key)) return -1
+    return this.#cache.get(key)?.[0] ?? -1
+  }
+
+  count(): number {
+    const xs = Array.from(this.#cache).filter(([key]) => !this.#isExpired(key))
+    return xs.length
+  }
+
+  #isExpired = (key: number) =>
+    this.#cache.has(key) && (this.#cache.get(key)?.[1] ?? Number.NEGATIVE_INFINITY) < Date.now()
+}
+
 export type F = (x: number) => number
 
 type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue }
