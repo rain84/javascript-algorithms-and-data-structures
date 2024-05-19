595f2bb5676b010e1431e1a362e51ca09793a1bd
rain84 <rainy_sky@mail.ru>
2024-05-19 23:14:12 +0300

    feat(LeetCode): ✨ add chunk

---
+++ src/leetcode/30-Days-of-JavaScript/chunk.ts
@@ -0,0 +1,32 @@
+/**
+ * 2677. Chunk Array
+ * https://leetcode.com/problems/chunk-array/description/
+ */
+export function chunk(arr: number[], size: number): number[][] {
+  const res: number[][] = []
+
+  for (let i = 0; i < arr.length; i += size) {
+    res.push(arr.slice(i, i + size))
+  }
+
+  return res
+}
+
+/** My 1st solution */
+function chunk2(arr: number[], size: number): number[][] {
+  const res: number[][] = []
+  let tmp: number[] = []
+
+  for (let i = 0; i < arr.length; i++) {
+    tmp.push(arr[i])
+
+    if ((i + 1) % size === 0) {
+      res.push(tmp)
+      tmp = []
+    }
+  }
+
+  if (tmp.length) res.push(tmp)
+
+  return res
+}
