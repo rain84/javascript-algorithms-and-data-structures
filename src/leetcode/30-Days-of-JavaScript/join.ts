65411310d7aeeb3c803e94306476788e70e7d9eb
rain84 <rainy_sky@mail.ru>
2024-05-20 00:21:19 +0300

    feat(LeetCode): ✨ add join

---
+++ src/leetcode/30-Days-of-JavaScript/join.ts
@@ -0,0 +1,20 @@
+/**
+ * 2722. Join Two Arrays by ID
+ * URL {@link https://leetcode.com/problems/join-two-arrays-by-id/description/}
+ */
+export function join(arr1: ArrayType[], arr2: ArrayType[]): ArrayType[] {
+  const r = (acc: Obj, x: ArrayType): Obj => ((acc[x.id] = x), acc)
+  const obj = arr1.reduce<Obj>(r, {})
+
+  for (const x of arr2) {
+    if (obj[x.id]) Object.assign(obj[x.id], x)
+    else obj[x.id] = x
+  }
+
+  return Object.values(obj)
+}
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue }
+type ArrayType = { id: number } & Record<string, JSONValue>
+
+type Obj = Record<number, ArrayType>
