77b9a3502ce82ad3d8d4cdf8336d302e1caf9892
rain84 <rainy_sky@mail.ru>
2024-05-20 01:47:37 +0300

    feat(LeetCode): ✨ add flat

---
+++ src/leetcode/30-Days-of-JavaScript/flat.ts
@@ -0,0 +1,18 @@
+/**
+ * 2625. Flatten Deeply Nested Array
+ * https://leetcode.com/problems/flatten-deeply-nested-array/description/
+ */
+export const flat = (arr: MultiDimensionalArray, n: number): MultiDimensionalArray => {
+  if (!n) return arr
+
+  const res: MultiDimensionalArray = []
+
+  for (const x of arr) {
+    if (Array.isArray(x) && n) res.push(...flat(x, n - 1))
+    else res.push(x)
+  }
+
+  return res
+}
+
+type MultiDimensionalArray = (number | MultiDimensionalArray)[]
