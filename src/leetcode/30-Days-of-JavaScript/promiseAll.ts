96ade92b5779f14910f18e76ce614e9f73541105
rain84 <rainy_sky@mail.ru>
2024-05-19 22:45:58 +0300

    feat(LeetCode): ✨ add promiseAll

---
+++ src/leetcode/30-Days-of-JavaScript/promiseAll.ts
@@ -0,0 +1,22 @@
+/**
+ * 2721. Execute Asynchronous Functions in Parallel
+ * URL {@link https://leetcode.com/problems/execute-asynchronous-functions-in-parallel/description/}
+ */
+export const promiseAll = <T>(arr: Fn<T>[]): Promise<T[]> =>
+  new Promise((resolve, reject) => {
+    const res: T[] = Array(arr.length)
+    let counter = res.length
+
+    for (let i = 0; i < arr.length; i++) {
+      const p = arr[i]()
+      const onResolve = (x: T): void => {
+        res[i] = x
+
+        if (!--counter) resolve(res)
+      }
+
+      p.then(onResolve, reject)
+    }
+  })
+
+type Fn<T> = () => Promise<T>
