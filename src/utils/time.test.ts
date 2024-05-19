373ff1c07a91e836faf0280bcd1d11fed43a3874
rain84 <rainy_sky@mail.ru>
2024-05-19 20:28:51 +0300

    refactor(utils): 🚚 replace resources

--- src/utils/time.test.ts
+++ src/utils/time.test.ts
@@ -1,4 +1,4 @@
-import { seconds } from './date'
+import { delay, seconds } from './time'
 
 it('seconds() should work', () => {
   const time = (Date.now() / 1000) | 0
@@ -8,4 +8,13 @@ it('seconds() should work', () => {
 it('seconds.last1000() should work', () => {
   const time = ((Date.now() / 1000) | 0) % 1000
   expect(seconds.last1000()).toBe(time)
+})
+
+it('wait() should work', async () => {
+  const time = 1000
+  const expired = ((Date.now() + time) / 1000) | 0
+  await delay(time)
+  const now = (Date.now() / 1000) | 0
+
+  expect(expired).toBe(now)
 })
--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,2 +1,5 @@
 export const seconds = () => (Date.now() / 1000) | 0
+
 seconds.last1000 = () => seconds() % 1000
+
+export const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
