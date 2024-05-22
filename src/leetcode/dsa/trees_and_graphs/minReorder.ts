5cc1283a1bfd7fd37d7f29bb14bd9d13e85fc4a9
rain84 <rainy_sky@mail.ru>
2024-05-22 21:24:54 +0300

    feat(LeetCode): ✨ added minReorder (lc. 1466)

--- src/leetcode/dsa/trees_and_graphs/minReorder.ts
+++ src/leetcode/dsa/trees_and_graphs/minReorder.ts
@@ -1,18 +1,16 @@
 /**
  * 1466. Reorder Routes to Make All Paths Lead to the City Zero
  * URL {@link https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/description/}
- *
- * @todo WORK IN PROGRESS
  */
 export function minReorder(n: number, connections: number[][]): number {
   const roads: Record<number, Set<number>> = {}
-  const paths: Record<number, number[]> = {}
+  const graph: Record<number, number[]> = {}
   const seen = new Set<number>()
 
   for (const [k, v] of connections) {
     ;(roads[k] ?? (roads[k] = new Set())).add(v)
-    ;(paths[k] ?? (paths[k] = [])).push(v)
-    ;(paths[v] ?? (paths[v] = [])).push(k)
+    ;(graph[k] ?? (graph[k] = [])).push(v)
+    ;(graph[v] ?? (graph[v] = [])).push(k)
   }
 
   const xs = [0]
@@ -22,12 +20,14 @@ export function minReorder(n: number, connections: number[][]): number {
     const x = xs.pop()!
 
     if (seen.has(x)) continue
-
     seen.add(x)
-    if (paths[x]) xs.push(...paths[x])
 
-    for (const neighbour of paths[x]) {
-      if (!roads?.[neighbour]?.has(x)) res++
+    if (graph[x]) xs.push(...graph[x])
+
+    for (const neighbour of graph[x]) {
+      if (!seen.has(neighbour) && !roads?.[neighbour]?.has(x)) {
+        res++
+      }
     }
   }
 
