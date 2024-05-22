36bf9004d70e62dfa3b5af6a8ca1d83369c604de
rain84 <rainy_sky@mail.ru>
2024-05-22 14:27:31 +0300

    feat(LeetCode): ✨ add findCircleNum (lc. 547)

---
+++ src/leetcode/dsa/trees_and_graphs/findCircleNum.ts
@@ -0,0 +1,94 @@
+/**
+ * 547. Number of Provinces
+ * URL {@link https://leetcode.com/problems/number-of-provinces/}
+ */
+
+/** Solved with DFS-recursion */
+export function findCircleNum(isConnected: number[][]): number {
+  const seen = new Set<number>()
+  const n = isConnected.length
+  let res = 0
+
+  const dfs = (v: number) => {
+    seen.add(v)
+
+    for (let j = 0; j < n; j++) {
+      if (isConnected[v][j] && !seen.has(j)) {
+        dfs(j)
+      }
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (seen.has(i)) continue
+
+    dfs(i)
+    res++
+  }
+
+  return res
+}
+
+/** Solved iteratively */
+export function findCircleNum2(isConnected: number[][]): number {
+  const seen = new Set<number>()
+  let res = 0
+
+  for (let vertex = 0; vertex < isConnected.length; vertex++) {
+    const stack = [isConnected[vertex]]
+
+    if (seen.has(vertex)) continue
+
+    while (stack.length) {
+      const neighbours = stack.pop()!
+
+      for (let n = 0; n < neighbours.length; n++) {
+        if (!neighbours[n] || seen.has(n)) continue
+
+        stack.push(isConnected[n])
+        seen.add(n)
+      }
+    }
+
+    res++
+  }
+
+  return res
+}
+
+/** My 1st solution :) */
+export function findCircleNum3(isConnected: number[][]): number {
+  // conversion to the matrix of [vertexes][[vertexes]]
+  const vertexes = new Map<number, number[]>()
+
+  for (let i = 0; i < isConnected.length; i++) {
+    const nodes: number[] = []
+
+    for (let j = 0; j < isConnected[i].length; j++) {
+      if (isConnected[i][j] === 1) nodes.push(j)
+    }
+
+    vertexes.set(i, nodes)
+  }
+
+  let res = 0
+
+  const seen = new Set<number>()
+
+  for (const [vertex, neighbours] of vertexes) {
+    if (seen.has(vertex)) continue
+
+    while (neighbours.length) {
+      const neighbour = neighbours.pop()!
+
+      if (seen.has(neighbour)) continue
+
+      neighbours.push(...vertexes.get(neighbour)!)
+      seen.add(neighbour)
+    }
+
+    res++
+  }
+
+  return res
+}
