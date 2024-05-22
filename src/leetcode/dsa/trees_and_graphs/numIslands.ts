6b3a5d4eb3b08468cafb589fa562b4c3d64a7f2c
rain84 <rainy_sky@mail.ru>
2024-05-22 16:54:32 +0300

    feat(LeetCode):

---
+++ src/leetcode/dsa/trees_and_graphs/numIslands.ts
@@ -0,0 +1,37 @@
+/**
+ *  200. Number of Islands
+ *  URL {@link https://leetcode.com/problems/number-of-islands/description/}
+ */
+
+export function numIslands(grid: string[][]): number {
+  // prevent mutation of the input, just rule of thumb
+  // grid = Array(grid.length)
+  //   .fill(0)
+  //   .map((_, i) => [...grid[i]])
+
+  let res = 0
+  const m = grid.length
+  const n = grid[0].length
+
+  const dfs = (i: number, j: number) => {
+    if (grid[i]?.[j] !== '1') return
+
+    grid[i][j] = '0'
+
+    dfs(i + 1, j)
+    dfs(i - 1, j)
+    dfs(i, j + 1)
+    dfs(i, j - 1)
+  }
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === '1') {
+        dfs(i, j)
+        res++
+      }
+    }
+  }
+
+  return res
+}
