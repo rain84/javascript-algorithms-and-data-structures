4c62d77efb1c5c4c6d82bc7c2468d01047355ee9
rain84 <rainy_sky@mail.ru>
2024-05-23 22:11:34 +0300

    feat(LeetCode): ✨ add reachableNodes (lc. 2368)

---
+++ src/leetcode/dsa/trees_and_graphs/reachableNodes.ts
@@ -0,0 +1,27 @@
+/**
+ * 2368. Reachable Nodes With Restrictions
+ * {@link https://leetcode.com/problems/reachable-nodes-with-restrictions/description/ | Link}
+ */
+export function reachableNodes(n: number, edges: number[][], restricted: number[]): number {
+  const graph: number[][] = Array.from({ length: n }, () => [])
+  const limited = new Set(restricted)
+  const seen = new Set<number>()
+
+  for (const [a, b] of edges) {
+    graph[a].push(b)
+    graph[b].push(a)
+  }
+
+  const dfs = (x: number): number => {
+    if (seen.has(x) || limited.has(x)) return 0
+
+    let res = 1
+    seen.add(x)
+
+    for (const i of graph[x]) res += dfs(i)
+
+    return res
+  }
+
+  return dfs(0)
+}
