0b4096421ee4ab578803e651179305653be1a0e0
rain84 <rainy_sky@mail.ru>
2024-05-23 16:35:04 +0300

    feat(LeetCode): ✨ add DFS solution for minReorder (lc. 1466)

--- src/leetcode/dsa/trees_and_graphs/minReorder.ts
+++ src/leetcode/dsa/trees_and_graphs/minReorder.ts
@@ -3,7 +3,38 @@
  *
  * {@link https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/description/ | Link}
  */
+
+/** DFS */
 export function minReorder(n: number, connections: number[][]): number {
+  const graph: [number, number][][] = Array.from({ length: n }, () => [])
+  const seen = new Set<number>()
+
+  for (const [a, b] of connections) {
+    // .push( [Neighbour, Road] )
+    graph[a].push([b, 1])
+    graph[b].push([a, 0])
+  }
+
+  const dfs = (x: number): number => {
+    if (seen.has(x)) return 0
+
+    seen.add(x)
+    let res = 0
+
+    // we should calculate all the roads from '0'
+    for (const [neighbour, road] of graph[x]) {
+      if (seen.has(neighbour)) continue
+      res += road + dfs(neighbour)
+    }
+
+    return res
+  }
+
+  return dfs(0)
+}
+
+/** BFS */
+export function minReorder2(n: number, connections: number[][]): number {
   const graph: [number, number][][] = Array.from({ length: n }, () => [])
   const seen = new Set<number>()
 
