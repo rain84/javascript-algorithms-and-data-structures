cf015196ba108507de1a001d1f7ae9f71d62f5f3
rain84 <rainy_sky@mail.ru>
2024-05-23 16:59:58 +0300

    feat(LeetCode): ✨ add DFS solution for canVisitAllRooms (lc. 841)

--- src/leetcode/dsa/trees_and_graphs/canVisitAllRooms.ts
+++ src/leetcode/dsa/trees_and_graphs/canVisitAllRooms.ts
@@ -2,7 +2,25 @@
  * 841. Keys and Rooms
  * {@link https://leetcode.com/problems/keys-and-rooms/description/ | Link}
  */
+
+/** DFS */
 export function canVisitAllRooms(rooms: number[][]): boolean {
+  const seen = new Set<number>()
+
+  const dfs = (i: number) => {
+    if (seen.has(i)) return
+    seen.add(i)
+
+    for (const j of rooms[i]) dfs(j)
+  }
+
+  dfs(0)
+
+  return rooms.length === seen.size
+}
+
+/** BFS */
+export function canVisitAllRoom2s(rooms: number[][]): boolean {
   const seen = new Set<number>()
   const q = [0]
 
