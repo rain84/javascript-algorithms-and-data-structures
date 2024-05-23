ca39d11ab77be6a5d77ec0481a3dccc3c623dcd0
rain84 <rainy_sky@mail.ru>
2024-05-23 20:14:54 +0300

    feat(LeetCode): ✨ add validPath (lc. 1971)

---
+++ src/leetcode/dsa/trees_and_graphs/validPath.test.ts
@@ -0,0 +1,28 @@
+import { validPath } from './validPath'
+
+it('should work 1', () => {
+  const n = 3
+  const edges = [
+    [0, 1],
+    [1, 2],
+    [2, 0],
+  ]
+  const source = 0
+  const destination = 2
+
+  expect(validPath(n, edges, source, destination)).toBeTruthy()
+})
+
+it('should work 2', () => {
+  const n = 6
+  const edges = [
+    [0, 1],
+    [0, 2],
+    [3, 5],
+    [5, 4],
+    [4, 3],
+  ]
+  const source = 0
+  const destination = 5
+  expect(validPath(n, edges, source, destination)).toBeFalsy()
+})
---
+++ src/leetcode/dsa/trees_and_graphs/validPath.ts
@@ -0,0 +1,60 @@
+/**
+ * 1971. Find if Path Exists in Graph
+ * {@link https://leetcode.com/problems/find-if-path-exists-in-graph | Link}
+ */
+
+/** DFS */
+export function validPath(
+  n: number,
+  edges: number[][],
+  source: number,
+  destination: number
+): boolean {
+  const graph: number[][] = Array.from({ length: n }, () => [])
+
+  for (const [a, b] of edges) {
+    graph[a].push(b)
+    graph[b].push(a)
+  }
+
+  const seen = new Set<number>()
+  const dfs = (i: number) => {
+    if (i === destination) return true
+    if (seen.has(i)) return false
+
+    seen.add(i)
+    return graph[i].some(dfs)
+  }
+
+  return dfs(source)
+}
+
+/** BFS */
+export function validPath2(
+  n: number,
+  edges: number[][],
+  source: number,
+  destination: number
+): boolean {
+  const graph: number[][] = Array.from({ length: n }, () => [])
+
+  for (const [a, b] of edges) {
+    graph[a].push(b)
+    graph[b].push(a)
+  }
+
+  const seen = new Set<number>()
+  const q = [source]
+
+  while (q.length) {
+    const i = q.pop()!
+
+    if (i === destination) return true
+    if (seen.has(i)) continue
+
+    seen.add(i)
+    q.push(...graph[i])
+  }
+
+  return false
+}
