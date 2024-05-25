a73cfb0933fc8f6d98cec708596bcaa4b065bee7
rain84 <rainy_sky@mail.ru>
2024-05-25 18:24:16 +0300

    feat(LeetCode): ✨ add distanceK (lc. 863)

---
+++ src/leetcode/dsa/trees_and_graphs/distanceK.test.ts
@@ -0,0 +1,20 @@
+import { TreeNode, createTree } from '../utils'
+import { distanceK } from './distanceK'
+
+it('should work 1', () => {
+  const root = [3, 5, 1, 6, 2, 0, 8, null, null, 7, 4]
+  //                         3
+  //             5                   1
+  //       6           2          0    8
+  // null    null    7   4
+  const target = new TreeNode(5)
+  const k = 2
+  const output = distanceK(createTree('3 [5 6 [2 7 4]] [1 0 8]'), target, k)
+  expect(output.sort()).toMatchObject([7, 4, 1].sort())
+})
+
+it('should work 2', () => {
+  const target = new TreeNode(1)
+  const k = 3
+  expect(distanceK(createTree('1'), target, k)).toMatchObject([])
+})
---
+++ src/leetcode/dsa/trees_and_graphs/distanceK.ts
@@ -0,0 +1,45 @@
+import type { TreeNode } from '../utils'
+
+/**
+ * 863. All Nodes Distance K in Binary Tree
+ * {@link https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/description/ | Link}
+ */
+export function distanceK(root: TreeNode | null, target: TreeNode | null, k: number): number[] {
+  if (!root) return [0]
+
+  const g: Record<number, number[]> = {}
+
+  const dfs = (node: TreeNode | null, parent: TreeNode | null = null) => {
+    if (!node) return
+
+    g[node.val] ??= []
+    if (parent) g[node.val].push(parent.val)
+    if (node.left) g[node.val].push(node.left.val)
+    if (node.right) g[node.val].push(node.right.val)
+
+    dfs(node.left, node)
+    dfs(node.right, node)
+  }
+
+  dfs(root)
+
+  const seen = new Set<number>()
+  let q = [target!.val]
+
+  while (q.length) {
+    if (!k--) return q
+
+    const nextQ: number[] = []
+
+    for (const x of q) {
+      if (seen.has(x)) continue
+
+      seen.add(x)
+      nextQ.push(...g[x].filter((x) => !seen.has(x)))
+    }
+
+    q = nextQ
+  }
+
+  return []
+}
