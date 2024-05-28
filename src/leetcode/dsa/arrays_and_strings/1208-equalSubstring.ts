406dec9bd364368c36a360830a90f8d86f5bc66a
rain84 <rainy_sky@mail.ru>
2024-05-29 00:16:14 +0300

    refactor(LeetCode): ♻️ improve equalSubstring (lc. 1208)

--- src/leetcode/dsa/arrays_and_strings/1208-equalSubstring.ts
+++ src/leetcode/dsa/arrays_and_strings/1208-equalSubstring.ts
@@ -5,17 +5,14 @@
 export function equalSubstring(s: string, t: string, maxCost: number): number {
   const getCost = (i: number) => Math.abs(s[i].charCodeAt(0) - t[i].charCodeAt(0))
   const n = s.length
-  let res = 0
-  let l = 0
-  let r = -1
   let cost = 0
+  let l = 0
 
-  while (++r < n) {
+  for (let r = 0; r < n; r++) {
     cost += getCost(r)
 
-    if (cost <= maxCost) res = Math.max(res, r - l + 1)
-    else cost -= getCost(l++)
+    if (cost > maxCost) cost -= getCost(l++)
   }
 
-  return res
+  return n - l
 }
