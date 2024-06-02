b4ddf99adb4da83c238363c03f1faae8e3b50fbd
rain84 <rainy_sky@mail.ru>
2024-06-02 14:38:59 +0300

    refactor(LeetCode): ⚡️ remove 'seen', use 'maze' instead (lc. 1926)

--- src/leetcode/dsa/trees_and_graphs/1926-nearestExit.ts
+++ src/leetcode/dsa/trees_and_graphs/1926-nearestExit.ts
@@ -5,13 +5,12 @@
 export function nearestExit(maze: string[][], entrance: number[]): number {
   const m = maze.length
   const n = maze[0].length
-  const seen = Array.from({ length: m }, () => Array.from({ length: n }, () => false))
   const dir = [0, 1, 0, -1, 0]
   const q = [[...entrance, 0]]
 
   for (const [i, j, steps] of q) {
-    if (seen[i][j]) continue
-    seen[i][j] = true
+    if (maze[i][j] === '+') continue
+    maze[i][j] = '+'
 
     for (let d = 0; d < 4; d++) {
       const [y, x] = [i + dir[d], j + dir[d + 1]]
