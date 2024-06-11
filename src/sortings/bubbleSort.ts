1ddc54a56c9220918d61af0b32fe527ab9aec1ea
rain84 <rainy_sky@mail.ru>
2024-06-11 14:02:49 +0300

    refactor(sorting): ♻️ update bubble sort

--- src/sortings/bubbleSort.ts
+++ src/sortings/bubbleSort.ts
@@ -11,18 +11,16 @@
                     2. If array is of large size but nearly sorted
     Remark          : Easiest to implement	
 */
-
 export const bubbleSort = (arr: number[]) => {
-  for (let i = arr.length - 1, haveSwap = true; haveSwap && i >= 0; i--) {
-    haveSwap = false
-    for (let j = 0; j < i; j++) {
-      if (arr[j] > arr[j + 1]) {
-        haveSwap = true
-        ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+  for (let j = arr.length - 1, isSorted = true; j > 0; j--, isSorted = true) {
+    for (let i = 0; i < j; i++) {
+      if (arr[i] > arr[i + 1]) {
+        ;[arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
+        isSorted = false
       }
     }
+    if (isSorted) break
   }
-
   return arr
 }
 
@@ -38,33 +36,6 @@ export const bubbleSort2 = (arr: number[], order = false) => {
       if (shouldSwap) {
         ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
       }
-    }
-  }
-
-  return arr
-}
-
-// improved performance on duplicated elements
-// NEED WORK
-// INPUT:  [5, 2, 8, 7, 4, 6, 4, 10, 2, 0]
-// SORTED: [2, 0, 2, 4, 4, 5, 6, 7, 8, 10]
-export const bubbleSortImproved = (arr: number[], order = false) => {
-  let i = arr.length
-  let notSorted = true
-
-  while (--i && notSorted) {
-    let j = -1
-    let swapCounter = 0
-
-    while (++j < i && notSorted) {
-      const shouldSwap = (order && arr[j] < arr[j + 1]) || (!order && arr[j] > arr[j + 1])
-
-      if (shouldSwap) {
-        ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
-        swapCounter++
-      }
-
-      if (swapCounter <= 1 && j === i - 1) notSorted = false
     }
   }
 
