e774a882085007771fb164d0d1a89b9797585e79
rain84 <rainy_sky@mail.ru>
2024-06-26 13:08:22 +0300

    feat(LeetCode): ✨ add balanceBST (lc. 1382)

---
+++ src/leetcode/dsa/trees_and_graphs/1382-balanceBST.test.ts
@@ -0,0 +1,23 @@
+import { createTree } from '../utils'
+import { balanceBST, balanceBST2 } from './1382-balanceBST'
+
+it('should work 1', () => {
+  // [1,null,2,null,3,null,4,null,null]
+  const tree = createTree('1 null [2 null [3 null 4]]')
+  const output = createTree('3 [2 1] 4')
+  expect(balanceBST2(tree)).toMatchObject(output!)
+})
+
+it('should work 2', () => {
+  // [2,1,3]
+  const tree = createTree('2 1 3')
+  const output = createTree('2 1 3')
+  expect(balanceBST2(tree)).toMatchObject(output!)
+})
+
+it('should work 3', () => {
+  // [1,null,2,null,3,null,4,null,null]
+  const tree = createTree('1 null [2 null [3 null 4]]')
+  const output = createTree('2 1 [3 null 4]')
+  expect(balanceBST(tree)).toMatchObject(output!)
+})
---
+++ src/leetcode/dsa/trees_and_graphs/1382-balanceBST.ts
@@ -0,0 +1,59 @@
+import { TreeNode } from '../utils'
+
+/**
+ * 1382. Balance a Binary Search Tree
+ * {@link https://leetcode.com/problems/balance-a-binary-search-tree/description/ | Link}
+ */
+export function balanceBST(root: TreeNode | null): TreeNode | null {
+  if (!root) return null
+
+  const a: number[] = []
+  const dfs = (node: TreeNode | null) => {
+    if (!node) return
+
+    dfs(node.left)
+    a.push(node.val)
+    dfs(node.right)
+  }
+  dfs(root)
+
+  const createBalancedTree = (l: number, r: number): TreeNode | null => {
+    const middle = (l + r) >> 1
+    if (r < l) return null
+
+    const left = createBalancedTree(l, middle - 1)
+    const right = createBalancedTree(middle + 1, r)
+
+    return new TreeNode(a[middle], left, right)
+  }
+  const res = createBalancedTree(0, a.length - 1)
+
+  return res
+}
+
+export function balanceBST2(root: TreeNode | null): TreeNode | null {
+  if (!root) return null
+
+  const nums: number[] = []
+  const dfs = (node: TreeNode | null) => {
+    if (!node) return
+
+    dfs(node.left)
+    nums.push(node.val)
+    dfs(node.right)
+  }
+  dfs(root)
+
+  const createBalancedTree = (nums: number[] = []): TreeNode | null => {
+    if (!nums.length) return null
+
+    const middle = nums.length >> 1
+    const left = createBalancedTree(nums.slice(0, middle))
+    const right = createBalancedTree(nums.slice(middle + 1))
+
+    return new TreeNode(nums[middle], left, right)
+  }
+  const res = createBalancedTree(nums)
+
+  return res
+}
