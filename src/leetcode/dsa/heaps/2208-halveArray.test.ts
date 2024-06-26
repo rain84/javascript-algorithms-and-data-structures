1e898397bceb5931b252214402a23e868fcce873
rain84 <rainy_sky@mail.ru>
2024-06-26 13:58:56 +0300

    feat(LeetCode): ✨ add halveArray (lc. 2208)

---
+++ src/leetcode/dsa/heaps/2208-halveArray.test.ts
@@ -0,0 +1,9 @@
+import { halveArray } from './2208-halveArray'
+
+it('should work 1', () => {
+  expect(halveArray([5, 19, 8, 1])).toBe(3)
+})
+
+it('should work 2', () => {
+  expect(halveArray([3, 8, 20])).toBe(3)
+})
---
+++ src/leetcode/dsa/heaps/2208-halveArray.ts
@@ -0,0 +1,22 @@
+import { MaxPriorityQueue } from '@datastructures-js/priority-queue'
+
+/**
+ * 2208. Minimum Operations to Halve Array Sum
+ * {@link https://leetcode.com/problems/minimum-operations-to-halve-array-sum/description/ | Link}
+ */
+export function halveArray(nums: number[]): number {
+  let sum = nums.reduce((a, b) => a + b) / 2
+  const maxQ = new MaxPriorityQueue<number>()
+  let res = 0
+
+  for (const x of nums) maxQ.enqueue(x)
+
+  while (sum > 0) {
+    const max = maxQ.dequeue().element / 2
+    sum -= max
+    maxQ.enqueue(max)
+    res++
+  }
+
+  return res
+}
