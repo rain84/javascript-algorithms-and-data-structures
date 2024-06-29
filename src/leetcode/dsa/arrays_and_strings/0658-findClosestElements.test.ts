17acc87d5c13881d0daf5335613163abb1382819
rain84 <rainy_sky@mail.ru>
2024-06-29 20:11:24 +0300

    feat(LeetCode): ✨ update findClosestElements (lc. 0658)

--- src/leetcode/dsa/arrays_and_strings/0658-findClosestElements.test.ts
+++ src/leetcode/dsa/arrays_and_strings/0658-findClosestElements.test.ts
@@ -1,4 +1,8 @@
-import { findClosestElements } from './0658-findClosestElements'
+import {
+  findClosestElements,
+  findClosestElements2,
+  findClosestElements3,
+} from './0658-findClosestElements'
 
 it.each(
   //  prettier-ignore
@@ -19,7 +23,13 @@ it.each(
       [0,0,1,2,3,3,4,7,7,8], 3, 5,
       [3,3,4]
     ],
+    [
+      [1,1,2,2,2,2,2,3,3], 3, 3,
+      [2,3,3]
+    ]
   ]
 )('should work %#', (arr, k, x, output) => {
   expect(findClosestElements(arr, k, x)).toMatchObject(output)
+  expect(findClosestElements2([...arr], k, x)).toMatchObject(output)
+  expect(findClosestElements3([...arr], k, x)).toMatchObject(output)
 })
--- src/leetcode/dsa/arrays_and_strings/0658-findClosestElements.ts
+++ src/leetcode/dsa/arrays_and_strings/0658-findClosestElements.ts
@@ -1,10 +1,36 @@
-import { MaxPriorityQueue } from '@datastructures-js/priority-queue'
-
 /**
  * 658. Find K Closest Elements
  * {@link https://leetcode.com/problems/find-k-closest-elements/description/ | Link}
  */
+/**
+ * 658. Find K Closest Elements
+ * {@link https://leetcode.com/problems/find-k-closest-elements/description/ | Link}
+ */
+/** Binary search */
 export function findClosestElements(arr: number[], k: number, x: number): number[] {
+  let [l, r] = [0, arr.length - k]
+
+  while (l < r) {
+    const mid = (l + r) >> 1
+    if (x - arr[mid] <= arr[mid + k] - x) r = mid
+    else l = mid + 1
+  }
+  return arr.slice(l, l + k)
+}
+
+/** Brute force with .sort() */
+export function findClosestElements2(arr: number[], k: number, x: number): number[] {
+  arr.sort((a, b) => {
+    const d1 = Math.abs(x - a)
+    const d2 = Math.abs(x - b)
+    if (d1 === d2) return a - b
+    return d1 - d2
+  })
+  return arr.slice(0, k).sort((a, b) => a - b)
+}
+
+/** Sliding window */
+export function findClosestElements3(arr: number[], k: number, x: number): number[] {
   let [l, r] = [0, arr.length]
 
   while (r - l > k) {
