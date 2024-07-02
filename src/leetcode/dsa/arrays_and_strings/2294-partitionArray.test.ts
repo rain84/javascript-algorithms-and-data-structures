20eac065d68099d1ec25c5153f9506d0a04ca70c
rain84 <rainy_sky@mail.ru>
2024-07-02 14:20:39 +0300

    feat(LeetCode): ✨ add partitionArray (lc. 2294)

--- src/leetcode/dsa/arrays_and_strings/2294-partitionArray.test.ts
+++ src/leetcode/dsa/arrays_and_strings/2294-partitionArray.test.ts
@@ -1,4 +1,4 @@
-import { partitionArray } from './2294-partitionArray'
+import { partitionArray, partitionArray2 } from './2294-partitionArray'
 
 it.each(
   //  prettier-ignore
@@ -18,4 +18,5 @@ it.each(
   ]
 )('should work %#', (nums, k, output) => {
   expect(partitionArray(nums, k)).toBe(output)
+  expect(partitionArray2(nums, k)).toBe(output)
 })
--- src/leetcode/dsa/arrays_and_strings/2294-partitionArray.ts
+++ src/leetcode/dsa/arrays_and_strings/2294-partitionArray.ts
@@ -3,6 +3,18 @@
  * {@link https://leetcode.com/problems/partition-array-such-that-maximum-difference-is-k/description/ | Link}
  */
 export function partitionArray(nums: number[], k: number): number {
+  let c = 0
+
+  while (nums.length) {
+    const max = Math.max(...nums) - k
+    nums = nums.filter((x) => x < max)
+    c++
+  }
+
+  return c
+}
+
+export function partitionArray2(nums: number[], k: number): number {
   nums.sort((a, b) => a - b)
   let c = 1
   let max = nums[0] + k
