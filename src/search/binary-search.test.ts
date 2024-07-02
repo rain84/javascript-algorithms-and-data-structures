3149e6f8bf4c6b1a1b57fdbb7120e077b00d648b
rain84 <rainy_sky@mail.ru>
2024-07-02 18:54:35 +0300

    feat(LeetCode): ✨ add binarySearch_Insert, binarySearch_LeftDuplicated

--- src/search/binary-search.test.ts
+++ src/search/binary-search.test.ts
@@ -1,14 +1,57 @@
-import { binarySearch } from './binary-search'
+import { binarySearch, binarySearch_Insert, binarySearch_LeftDuplicated } from './binary-search'
 
 const a = [0, 1, 2, 4, 22, 27, 28, 37, 42, 46, 47, 56, 60, 61, 67, 77, 85, 92, 98, 100]
+const a2 = [4, 19, 22, 60, 60, 63, 66, 67, 69, 69, 70, 76, 83, 85, 85, 86, 90, 95, 98, 100]
 
-it('should work 1', () => {
-  expect(binarySearch(a, 28)).toBe(a.indexOf(28))
-  expect(binarySearch(a, 61)).toBe(a.indexOf(61))
-  expect(binarySearch(a, 0)).toBe(a.indexOf(0))
-  expect(binarySearch(a, 100)).toBe(a.indexOf(100))
+it.each(
+  //  prettier-ignore
+  [
+    [
+      a, 28,
+    ],
+    [
+      a, 61,
+    ],
+    [
+      a, 0,
+    ],
+    [
+      a, 100,
+    ],
+    [
+      a, 9999,
+    ],
+  ]
+)('binarySearch should work %#', (arr, x) => {
+  expect(binarySearch(arr, x)).toBe(a.indexOf(x))
 })
 
-it('should work 2', () => {
-  expect(binarySearch(a, 9999)).toBe(a.indexOf(9999))
+it.each(
+  //  prettier-ignore
+  [
+    [
+      a2, 69,
+      70
+    ],
+    [
+      a2, 22,
+      60
+    ],
+  ]
+)('binarySearch_Insert should work %#', (arr, x, output) => {
+  expect(binarySearch_Insert(arr, x)).toBe(a2.indexOf(output))
+})
+
+it.each(
+  //  prettier-ignore
+  [
+    [
+      a2, 69,
+    ],
+    [
+      a2, 85,
+    ],
+  ]
+)('binarySearch_LeftDuplicated should work %#', (arr, x) => {
+  expect(binarySearch_LeftDuplicated(arr, x)).toBe(a2.indexOf(x))
 })
--- src/search/binary-search.ts
+++ src/search/binary-search.ts
@@ -1,3 +1,4 @@
+/** Find index of 'target' */
 export const binarySearch = (sortedArr: number[], num: number) => {
   let [l, r] = [0, sortedArr.length - 1]
   let i = 0
@@ -11,4 +12,30 @@ export const binarySearch = (sortedArr: number[], num: number) => {
     if (sortedArr[i] < num) l = i + 1
     else r = i
   }
+}
+
+/** Find the index to insert 'target' to preserve sorting order */
+export const binarySearch_Insert = (arr: number[], target: number) => {
+  let [l, r] = [0, arr.length - 1]
+
+  while (l < r) {
+    const mid = (l + r) >> 1
+    if (arr[mid] > target) r = mid
+    else l = mid + 1
+  }
+
+  return l
+}
+
+/** Find leftmost index of duplicated 'target' */
+export const binarySearch_LeftDuplicated = (arr: number[], target: number) => {
+  let [l, r] = [0, arr.length - 1]
+
+  while (l < r) {
+    const mid = (l + r) >> 1
+    if (arr[mid] >= target) r = mid
+    else l = mid + 1
+  }
+
+  return l
 }
