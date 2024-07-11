f5f277e717639c6de725630a428e8d32881b0167
rain84 <rainy_sky@mail.ru>
2024-07-11 23:20:26 +0300

    refactor(LeetCode): ♻️ improve solution (lc. 0198)

--- src/leetcode/dsa/dynamic-programming/0198-rob.ts
+++ src/leetcode/dsa/dynamic-programming/0198-rob.ts
@@ -4,37 +4,24 @@
  */
 export function rob(nums: number[]): number {
   const n = nums.length
-  const cache = Array(n + 1).fill(0)
-  cache[1] = nums[0]
+  const f = Array(n + 1).fill(0)
+  f[1] = nums[0]
 
   for (let i = 2; i < n + 1; i++) {
-    cache[i] = Math.max(nums[i - 1] + cache[i - 2], cache[i - 1])
+    f[i] = Math.max(nums[i - 1] + f[i - 2], f[i - 1])
   }
 
-  return cache[n]
+  return f[n]
 }
 
 export function rob2(nums: number[]): number {
-  const cache: Record<number, number> = {}
   const n = nums.length
-  let res = 0
+  const f = Array(n).fill(0)
 
-  const dp = (i: number) => {
-    if (cache[i] !== undefined) return cache[i]
-
-    let max = 0
-    for (let j = i + 2; j < n; j++) {
-      max = Math.max(max, dp(j))
-    }
-
-    cache[i] = max + nums[i]
-
-    return cache[i]
+  const dfs = (i: number): number => {
+    if (i >= n) return 0
+    return f[i] || (f[i] = Math.max(nums[i] + dfs(i + 2), dfs(i + 1)))
   }
 
-  for (let i = 0; i < n; i++) {
-    res = Math.max(res, dp(i))
-  }
-
-  return res
+  return dfs(0)
 }
