e77248b9ad952dc9e73bb2e9e8a47f7c54b67f26
rain84 <rainy_sky@mail.ru>
2024-07-22 12:44:26 +0300

    feat(LeetCode): 🚧 add buildMatrix (lc. 2302) (WIP)

---
+++ src/leetcode/dsa/trees_and_graphs/2392-buildMatrix.ts
@@ -0,0 +1,9 @@
+/**
+ * 2392. Build a Matrix With Conditions
+ */
+/** TODO implement task */
+export function buildMatrix(k: number, rowConds: number[][], colConds: number[][]): number[][] {
+  const res = Array.from({ length: k }, () => Array(k).fill(0))
+
+  return res
+}
