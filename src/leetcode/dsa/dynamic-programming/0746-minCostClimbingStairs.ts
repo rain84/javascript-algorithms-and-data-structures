2b15a94d7a002969980a27ef571241700b034387
rain84 <rainy_sky@mail.ru>
2024-07-31 20:20:34 +0300

    refactor(LeetCode): ♻️ rename variable

--- src/leetcode/dsa/dynamic-programming/0746-minCostClimbingStairs.ts
+++ src/leetcode/dsa/dynamic-programming/0746-minCostClimbingStairs.ts
@@ -4,13 +4,13 @@
  */
 export function minCostClimbingStairs(cost: number[]): number {
   const n = cost.length
-  const cache = Array(n + 1).fill(0)
+  const f = Array(n + 1).fill(0)
 
   for (let i = 2; i <= n; i++) {
-    cache[i] = Math.min(cost[i - 1] + cache[i - 1], cost[i - 2] + cache[i - 2])
+    f[i] = Math.min(cost[i - 1] + f[i - 1], cost[i - 2] + f[i - 2])
   }
 
-  return cache[n]
+  return f[n]
 }
 
 export function minCostClimbingStairs2(cost: number[]): number {
