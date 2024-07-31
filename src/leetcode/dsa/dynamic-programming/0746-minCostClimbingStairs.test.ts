1980df4b83807e0310234734ea3e17f9ecb83052
rain84 <rainy_sky@mail.ru>
2024-07-31 20:49:54 +0300

    test(LeetCode): ✅ add tests (lc. 0746)

--- src/leetcode/dsa/dynamic-programming/0746-minCostClimbingStairs.test.ts
+++ src/leetcode/dsa/dynamic-programming/0746-minCostClimbingStairs.test.ts
@@ -1,4 +1,8 @@
-import { minCostClimbingStairs, minCostClimbingStairs2 } from './0746-minCostClimbingStairs'
+import {
+  minCostClimbingStairs,
+  minCostClimbingStairs2,
+  minCostClimbingStairs3,
+} from './0746-minCostClimbingStairs'
 
 it.each(
   //  prettier-ignore
@@ -10,9 +14,14 @@ it.each(
     [
       [1,100,1,1,1,100,1,1,100,1],
       6
+    ],
+    [
+      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0],
+      1
     ]
   ]
 )('should work %#', (input, output) => {
   expect(minCostClimbingStairs(input)).toBe(output)
   expect(minCostClimbingStairs2(input)).toBe(output)
+  expect(minCostClimbingStairs3(input)).toBe(output)
 })
