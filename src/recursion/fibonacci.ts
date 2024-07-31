f59e04b56716788274bf00190e95701572bd8492
rain84 <rainy_sky@mail.ru>
2024-07-31 17:55:03 +0300

    refactor: ♻️ update fibonacci

--- src/recursion/fibonacci.ts
+++ src/recursion/fibonacci.ts
@@ -1,46 +1,13 @@
-//  fibonacci via recursion
-export const fib1 = (() => {
-  const cache = new Map()
+export const fib1 = (n: number): number => (n <= 1 ? n : fib1(n - 1) + fib1(n - 2))
 
-  const memoized = (n: number) => {
-    if (!cache.has(n)) {
-      const val = n <= 1 ? n : fib1(n - 1) + fib1(n - 2)
-      cache.set(n, val)
-    }
+/**  */
+export const fib2 = (n: number) => {
+  const arr = Array(n + 1)
+  ;[arr[0], arr[1]] = [0, 1]
 
-    return cache.get(n)
+  for (let i = 2; i <= n; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2]
   }
 
-  return memoized
-})()
-
-//  fibonacci via while-iteration
-export const fib2 = (() => {
-  const cache = [0, 1]
-
-  const memoized = (n: number) => {
-    if (n < 0) return
-
-    while (cache[n] === undefined) cache.push(cache[cache.length - 1] + cache[cache.length - 2])
-
-    return cache[n]
-  }
-
-  return memoized
-})()
-
-export const fib3 = (n: number) => {
-  let prev1 = 0
-  let prev2 = 1
-  let res = 0
-  if (n === 0) return prev1
-  if (n === 1) return prev2
-
-  while (--n) {
-    res = prev1 + prev2
-    prev1 = prev2
-    prev2 = res
-  }
-
-  return res
+  return arr[n]
 }
