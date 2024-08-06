59452634a7dc057c84d22beabcf492a968502469
rain84 <rainy_sky@mail.ru>
2024-08-06 20:22:11 +0300

    refactor(LeetCode): ♻️ update rob (lc. 198)

--- src/leetcode/dsa/dynamic-programming/0198-rob.ts
+++ src/leetcode/dsa/dynamic-programming/0198-rob.ts
@@ -1,26 +1,43 @@
 /**
  * 198. House Robber
  * {@link https://leetcode.com/problems/house-robber/ | Link}
+ *
+ * Topics: Array | Dynamic Programming
+ *
+ * Performance
+ * ───────────────────────────────────────────────────────────────────────────
+ *  (index)  Function      ops/sec  Time (ms)  Iterations  Speed (%)
+ * ───────────────────────────────────────────────────────────────────────────
+ *  1        rob           2214696  45         100000      100.00
+ *  2        rob2          669214   149        100000      30.22
+ * ───────────────────────────────────────────────────────────────────────────
  */
+
+/** Bottom-up approach, tabulation */
 export function rob(nums: number[]): number {
   const n = nums.length
-  const f = Array(n + 1).fill(0)
-  f[1] = nums[0]
 
-  for (let i = 2; i <= n; i++) {
-    f[i] = Math.max(nums[i - 1] + f[i - 2], f[i - 1])
+  if (n === 1) return nums[0]
+
+  let prev = nums[0]
+  let res = Math.max(nums[0], nums[1])
+
+  for (let i = 2; i < n; i++) {
+    ;[res, prev] = [Math.max(res, prev + nums[i]), res]
   }
 
-  return f[n]
+  return res
 }
 
+/** Top-down approach, recursion */
 export function rob2(nums: number[]): number {
   const n = nums.length
-  const f = Array(n).fill(0)
+  const f = Array(n).fill(-1)
 
   const dfs = (i: number): number => {
     if (i >= n) return 0
-    return f[i] || (f[i] = Math.max(nums[i] + dfs(i + 2), dfs(i + 1)))
+    if (f[i] === -1) f[i] = Math.max(nums[i] + dfs(i + 2), dfs(i + 1))
+    return f[i]
   }
 
   return dfs(0)
