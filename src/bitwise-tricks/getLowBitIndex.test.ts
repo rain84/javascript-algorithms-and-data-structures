8a99eec164144b5c8d44b281b757371aa6caa9bc
rain84 <rainy_sky@mail.ru>
2024-08-07 23:21:26 +0300

    feat(bitwise tricks): ✨ add getLowBitIndex

---
+++ src/bitwise-tricks/getLowBitIndex.test.ts
@@ -0,0 +1,18 @@
+import { getLowBitIndex } from './getLowBitIndex'
+
+it.each(
+  //  prettier-ignore
+  [
+    [0, -1],  // '0'
+    [1,  0],  // '1'
+    [10, 1],  // '1010'
+    [11, 0],  // '1011'
+    [12, 2],  // '1100'
+    [13, 0],  // '1101'
+    [14, 1],  // '1110'
+    [15, 0],  // '1111'
+    [16, 4],  // '10000'
+  ]
+)('should work %#', (input, output) => {
+  expect(getLowBitIndex(input)).toBe(output)
+})
---
+++ src/bitwise-tricks/getLowBitIndex.ts
@@ -0,0 +1,8 @@
+export const getLowBitIndex = (x: number) => {
+  if (!x) return -1
+
+  let c = 1
+  while ((x & ((1 << c) - 1)) === 0) c++
+
+  return c - 1
+}
