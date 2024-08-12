6ade4e9d9c509fbd29b602eaa7d876be4385bbc2
rain84 <rainy_sky@mail.ru>
2024-08-12 12:54:42 +0300

    refactor(LeetCode): ♻️ update solution (lc. 0703)

--- src/leetcode/dsa/heaps/0703-KthLargest.ts
+++ src/leetcode/dsa/heaps/0703-KthLargest.ts
@@ -3,29 +3,21 @@ import { MinPriorityQueue } from '@datastructures-js/priority-queue'
 /**
  * 703. Kth Largest Element in a Stream
  * {@link https://leetcode.com/problems/kth-largest-element-in-a-stream/ | Link}
+ *
+ * Topics: Tree | Design | Binary Search Tree | Heap (Priority Queue) | Binary Tree | Data Stream
  */
 export class KthLargest {
   #pq = new MinPriorityQueue<number>()
-  #k = 0
+  #k = 1
 
   constructor(k: number, nums: number[]) {
     this.#k = k
-    for (const x of nums) {
-      this.#pq.enqueue(x)
-      if (this.#pq.size() > k) this.#pq.dequeue()
-    }
+    for (const x of nums) this.add(x)
   }
 
   add(val: number): number {
     this.#pq.enqueue(val)
     if (this.#pq.size() > this.#k) this.#pq.dequeue()
-
     return this.#pq.front().element
   }
 }
-
-/**
- * Your KthLargest object will be instantiated and called as such:
- * var obj = new KthLargest(k, nums)
- * var param_1 = obj.add(val)
- */
