8be1fa382d197e15251b6f7207e99ca59c801967
rain84 <rainy_sky@mail.ru>
2024-08-13 11:38:28 +0300

    refactor(LeetCode): ⚰️ remove dead code (lc. 1482)

--- src/leetcode/dsa/arrays_and_strings/1482-minDays.ts
+++ src/leetcode/dsa/arrays_and_strings/1482-minDays.ts
@@ -1,30 +1,9 @@
 /**
  * 1482. Minimum Number of Days to Make m Bouquets
  * {@link https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/ | Link}
+ *
+ * Topics: Array | Binary Search
+ *
+ * TODO: WIP
  */
-export function minDays(bloomDay: number[], m: number, k: number): number {
-  const n = m * k
-  if (n > bloomDay.length) return -1
-  const cnt: number[][] = []
-  const indexes = [...new Set(bloomDay)].reduce<Record<number, number[]>>(
-    (acc, val) => ((acc[val] = []), acc),
-    {}
-  )
-
-  for (let i = 1, prev = bloomDay[0], c = 1; i < bloomDay.length; i++) {
-    if (bloomDay[i] === prev) c++
-    else {
-      cnt[prev]
-    }
-  }
-
-  let count = 0
-  for (const [day, c] of Object.entries(cnt)) {
-    count += c
-    if (count >= n) return +day
-  }
-
-  return -1
-}
-
-minDays([1, 10, 3, 10, 2], 3, 1)
+export function minDays(bloomDay: number[], m: number, k: number): number {}
