cdaa6f501d9d99ddd9da33f71802dad4aae71df4
rain84 <rainy_sky@mail.ru>
2024-08-23 18:55:56 +0300

    test(utils): ✅ update tests for utils

--- src/crypto/utils.test.ts
+++ src/crypto/utils.test.ts
@@ -1,32 +1,56 @@
-import { greatestCommonDivisor, isCoprime, euler } from './utils'
+import { euler, gcd, isCoprime } from './utils'
 
-it.each`
-  a        | b          | gcd
-  ${54}    | ${24}      | ${6}
-  ${180}   | ${150}     | ${30}
-  ${23444} | ${1976232} | ${4}
-`('%# gCD($a, $b) should be equal $gcd', ({ a, b, gcd }) => {
-  expect(greatestCommonDivisor(a, b)).toBe(gcd)
-  expect(greatestCommonDivisor(b, a)).toBe(gcd)
+it.each(
+  //  prettier-ignore
+  [
+    [
+      54, 24,
+      6
+    ],
+    [
+      180, 150,
+      30
+    ],
+    [
+      23444, 1976232,
+      4
+    ],
+  ]
+)('gcd() should work %#', (a, b, output) => {
+  expect(gcd(a, b)).toBe(output)
+  expect(gcd(b, a)).toBe(output)
 })
 
-it.each`
-  a     | b      | res
-  ${14} | ${26}  | ${false}
-  ${13} | ${7}   | ${true}
-  ${41} | ${347} | ${true}
-`('%# isCoprime($a, $b) should be equal $res', ({ a, b, res }) => {
-  expect(isCoprime(a, b)).toBe(res)
+it.each(
+  //  prettier-ignore
+  [
+    [
+      14, 26,
+      false
+    ],
+    [
+      13, 7,
+      true
+    ],
+    [
+      41, 347,
+      true
+    ]
+  ]
+)('isCoprime() should work %#', (a, b, output) => {
+  expect(isCoprime(a, b)).toBe(output)
 })
 
-it.each`
-  x      | res
-  ${1}   | ${1}
-  ${5}   | ${4}
-  ${101} | ${100}
-  ${201} | ${132}
-  ${301} | ${252}
-  ${401} | ${400}
-`('%# euler($x, $b) should be equal $res', ({ x, res }) => {
-  expect(euler(x)).toBe(res)
+it.each(
+  //  prettier-ignore
+  [
+    [1, 1],
+    [5, 4],
+    [101, 100],
+    [201, 132],
+    [301, 252],
+    [401, 400],
+  ]
+)('euler() should work %#', (input, output) => {
+  expect(euler(input)).toBe(output)
 })
