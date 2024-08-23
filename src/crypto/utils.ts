a6782a7d9b9943cd48af20686a2be2d499129804
rain84 <rainy_sky@mail.ru>
2024-08-23 18:55:13 +0300

    refactor(utils): ♻️ update gcd (greatest common divisor)

--- src/crypto/utils.ts
+++ src/crypto/utils.ts
@@ -1,11 +1,8 @@
-export const greatestCommonDivisor = (a: number, b: number): number => {
-  if (b < a) [a, b] = [b, a]
-  const gcd = (a: number, b: number): number => (a ? gcd(b % a, a) : b)
-  return gcd(a, b)
-}
+/** greatest common divisor */
+export const gcd = (a: number, b: number): number => (a ? gcd(b % a, a) : b)
 
 export const isCoprime = (a: number, b: number) => {
-  return greatestCommonDivisor(a, b) === 1
+  return gcd(a, b) === 1
 }
 
 export const euler = (x: number) => {
