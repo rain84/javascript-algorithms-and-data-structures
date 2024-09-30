6c4961764b67f92e8615f799fab1f0986c4ffe73
rain84 <rainy_sky@mail.ru>
2024-09-30 14:38:14 +0300

    feat(LeetCode): ✨ add CustomStack, solved with Lazy Propagation (lc. 1381)

--- src/leetcode/dsa/arrays_and_strings/1381-CustomStack.test.ts
+++ src/leetcode/dsa/arrays_and_strings/1381-CustomStack.test.ts
@@ -1,8 +1,27 @@
-import { CustomStack } from './1381-CustomStack'
+import { CustomStack, CustomStack2 } from './1381-CustomStack'
 
-it('should work 1', () => {
+it('CustomStack should work', () => {
   const res: number[] = []
   const stk = new CustomStack(3)
+  stk.push(1)
+  stk.push(2)
+  res.push(stk.pop())
+  stk.push(2)
+  stk.push(3)
+  stk.push(4)
+  stk.increment(5, 100)
+  stk.increment(2, 100)
+  res.push(stk.pop())
+  res.push(stk.pop())
+  res.push(stk.pop())
+  res.push(stk.pop())
+
+  expect(res).toMatchObject([2, 103, 202, 201, -1])
+})
+
+it('CustomStack2 should work', () => {
+  const res: number[] = []
+  const stk = new CustomStack2(3)
   stk.push(1)
   stk.push(2)
   res.push(stk.pop())
--- src/leetcode/dsa/arrays_and_strings/1381-CustomStack.ts
+++ src/leetcode/dsa/arrays_and_strings/1381-CustomStack.ts
@@ -3,7 +3,54 @@
  *
  * Topics: Array | Stack | Design
  */
+
+/**
+ * Array using Lazy Propagation
+ * Time complexity: O(1) for all operations
+ * */
 export class CustomStack {
+  #maxSize: number
+  #stk: number[]
+  #inc: number[]
+  #i = 0
+
+  constructor(maxSize: number) {
+    this.#maxSize = maxSize
+    this.#stk = Array(maxSize).fill(0)
+    this.#inc = Array(maxSize).fill(0)
+  }
+
+  push(x: number): void {
+    if (this.#i < this.#maxSize) {
+      this.#stk[this.#i++] = x
+    }
+  }
+
+  pop(): number {
+    if (this.#i === 0) return -1
+
+    this.#i--
+    const val = this.#stk[this.#i] + this.#inc[this.#i]
+
+    if (this.#i) {
+      this.#inc[this.#i - 1] += this.#inc[this.#i]
+    }
+    this.#inc[this.#i] = 0
+
+    return val
+  }
+
+  increment(k: number, val: number): void {
+    if (this.#i) {
+      this.#inc[Math.min(k, this.#i) - 1] += val
+    }
+  }
+}
+
+/**
+ * Time complexity: O(1) for push and pop, O(k) for increment
+ */
+export class CustomStack2 {
   #maxSize: number
   #stk: number[] = []
 
