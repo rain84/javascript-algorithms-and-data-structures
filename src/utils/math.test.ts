5e7045fbf4018d37aa9184576d5fd00a981a9339
rain84 <rainy_sky@mail.ru>
2024-11-04 15:08:41 +0300

    feat(utils): ✨ add transpose util for 2D-matix

--- src/utils/math.test.ts
+++ src/utils/math.test.ts
@@ -1,4 +1,4 @@
-import { getDigit, getLength } from './math'
+import { getDigit, getLength, transpose } from './math'
 
 it('getDigit() should work', () => {
   expect(getDigit(123, 1)).toBe(3)
@@ -9,4 +9,18 @@ it('getDigit() should work', () => {
   expect(getLength(1)).toBe(1)
   expect(getLength(10)).toBe(2)
   expect(getLength(-100)).toBe(3)
+})
+
+it('rotate() should rotate matrix', () => {
+  const matrix = [
+    [1, 2],
+    [3, 4],
+    [5, 6],
+  ]
+  expect(transpose(matrix)).toMatchObject([
+    [1, 3, 5],
+    [2, 4, 6],
+  ])
+
+  expect(transpose(transpose(matrix))).toMatchObject(matrix)
 })
--- src/utils/math.ts
+++ src/utils/math.ts
@@ -4,3 +4,17 @@ export const getDigit = (num: number, position: number) =>
 export const getLength = (num: number) => (num === 0 ? 1 : ~~Math.log10(Math.abs(num)) + 1)
 
 export const random = (max = 0, min = 0) => Math.floor(Math.random() * (max - min + 1)) + min
+
+/** Transpose matrix */
+export const transpose = <T>(matrix: T[][]): T[][] => {
+  const [m, n] = [matrix.length, matrix[0].length]
+  const res = Array.from({ length: n }, () => Array<T>(m))
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      res[i][j] = matrix[j][i]
+    }
+  }
+
+  return res
+}
